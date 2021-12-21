c67d52574895f3fc5c1b2f331cb1767292e92200
chen.levin <chen.levin@ironsrc.com>
2021-12-21 13:19:44 +0200

    fix(v13): remove circle dep on dropdown option directive

--- projects/fusion-ui/src/lib/components/ui-components/dropdown/dropdown-option/dropdown-option.module.ts
+++ projects/fusion-ui/src/lib/components/ui-components/dropdown/dropdown-option/dropdown-option.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {DropdownOptionComponent} from './dropdown-option.component';
-import {DropdownOptionDirective} from './dropdown-option.directive';
+import {DropdownOptionComponent, DropdownOptionDirective} from './dropdown-option.component';
 import {IconModule} from '../../icon/icon.module';
 import {TooltipModule} from '../../tooltip/tooltip.module';
 import {FlagModule} from '../../flag/flag.module';
