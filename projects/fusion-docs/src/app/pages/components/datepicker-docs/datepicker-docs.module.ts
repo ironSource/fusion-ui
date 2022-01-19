ef28500880f96899ef07c5184ee79bc0d3ff7c7e
AndyKIron <andyk@is.com>
2022-01-19 14:24:01 +0200

    chore(fu-309): fix lint

--- projects/fusion-docs/src/app/pages/components/datepicker-docs/datepicker-docs.module.ts
+++ projects/fusion-docs/src/app/pages/components/datepicker-docs/datepicker-docs.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 
 import {DatepickerDocsRoutingModule} from './datepicker-docs-routing.module';
 import {DatepickerDocsComponent} from './datepicker-docs.component';
-import {DatepickerModule} from '@ironsource/fusion-ui';
+import {DatepickerModule} from 'projects/fusion-ui/src/public-api';
 import {ExampleBlockModule} from '../../../components/example-block/example-block.module';
 import {CodeBlockModule} from '../../../components/code-block/code-block.module';
 import {DocsMenuModule} from '../../../components/docs-menu/docs-menu.module';
