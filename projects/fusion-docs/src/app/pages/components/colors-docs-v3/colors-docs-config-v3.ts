0ef4d7cead7ae7476a7cb79751e9d395aecda7ef
AndyKIron <andyk@is.com>
2022-04-04 17:28:14 +0300

    feat(fu-371): done with old doc color v3 page

--- projects/fusion-docs/src/app/pages/components/colors-docs-v3/colors-docs-config-v3.ts
+++ projects/fusion-docs/src/app/pages/components/colors-docs-v3/colors-docs-config-v3.ts
@@ -82,221 +82,248 @@ export const THEME_COLORS_PALETTE_V3: Array<any> = [
                 value: '#626970'
             }
         ]
-    }
-    // {
-    //     name: 'Dark',
-    //     className: 'fu-dark',
-    //     colors: [
-    //         {
-    //             name: ' $fu-dark-100',
-    //             className: 'fu-dark-100',
-    //             value: '#ced0e0'
-    //         },
-    //         {
-    //             name: '$fu-dark-200',
-    //             className: 'fu-dark-200',
-    //             value: '#4c548e'
-    //         },
-    //         {
-    //             name: '$fu-dark-300',
-    //             className: 'fu-dark-300',
-    //             value: '#313d91'
-    //         },
-    //         {
-    //             name: '$fu-dark-400',
-    //             className: 'fu-dark-400',
-    //             value: '#212791'
-    //         },
-    //         {
-    //             name: '$fu-dark-500',
-    //             className: 'fu-dark-500',
-    //             value: '#0d148c'
-    //         },
-    //         {
-    //             name: '$fu-dark-600',
-    //             className: 'fu-dark-600',
-    //             value: '#021074'
-    //         },
-    //         {
-    //             name: '$v2-2dark-700',
-    //             className: 'fu-dark-700',
-    //             value: '#1d1c64'
-    //         }
-    //     ]
-    // },
-    // {
-    //     name: 'Primary',
-    //     className: 'fu-primary',
-    //     colors: [
-    //         {
-    //             name: '$fu-primary-100',
-    //             className: 'fu-primary-100',
-    //             value: '#e2eeff'
-    //         },
-    //         {
-    //             name: '$fu-primary-200',
-    //             className: 'fu-primary-200',
-    //             value: '#c9dfff'
-    //         },
-    //         {
-    //             name: '$fu-primary-300',
-    //             className: 'fu-primary-300',
-    //             value: '#96c0ff'
-    //         },
-    //         {
-    //             name: '$fu-primary-400',
-    //             className: 'fu-primary-400',
-    //             value: '#68a5ff'
-    //         },
-    //         {
-    //             name: '$fu-primary-500',
-    //             className: 'fu-primary-500',
-    //             value: '#3083ff'
-    //         },
-    //         {
-    //             name: '$fu-primary-600',
-    //             className: 'fu-primary-600',
-    //             value: '#005be2'
-    //         }
-    //     ]
-    // },
-    // {
-    //     name: 'Secondary',
-    //     className: 'fu-secondary',
-    //     colors: [
-    //         {
-    //             name: '$fu-secondary-100',
-    //             className: 'fu-secondary-100',
-    //             value: '#e1fbfc'
-    //         },
-    //         {
-    //             name: '$fu-secondary-200',
-    //             className: 'fu-secondary-200',
-    //             value: '#bff2f5'
-    //         },
-    //         {
-    //             name: '$fu-secondary-300',
-    //             className: 'fu-secondary-300',
-    //             value: '#91e2e7'
-    //         },
-    //         {
-    //             name: '$fu-secondary-500',
-    //             className: 'fu-secondary-500',
-    //             value: '#00ccd7'
-    //         }
-    //     ]
-    // },
-    // {
-    //     name: 'Positive',
-    //     className: 'fu-positive',
-    //     colors: [
-    //         {
-    //             name: '$fu-positive-100',
-    //             className: 'fu-positive-100',
-    //             value: '#daf5ec'
-    //         },
-    //         {
-    //             name: '$fu-positive-200',
-    //             className: 'fu-positive-200',
-    //             value: '#b4e9d2'
-    //         },
-    //         {
-    //             name: '$fu-positive-300',
-    //             className: 'fu-positive-300',
-    //             value: '#78ddbc'
-    //         },
-    //         {
-    //             name: '$fu-positive-400',
-    //             className: 'fu-positive-400',
-    //             value: '#5cd6ad'
-    //         },
-    //         {
-    //             name: '$fu-positive-500',
-    //             className: 'fu-positive-500',
-    //             value: '#22c891'
-    //         },
-    //         {
-    //             name: '$fu-positive-600',
-    //             className: 'fu-positive-600',
-    //             value: '#1eb281'
-    //         },
-    //         {
-    //             name: '$fu-positive-700',
-    //             className: 'fu-positive-700',
-    //             value: '#1b9c71'
-    //         }
-    //     ]
-    // },
-    // {
-    //     name: 'Negative',
-    //     className: 'fu-negative',
-    //     colors: [
-    //         {
-    //             name: '$fu-negative-100',
-    //             className: 'fu-negative-100',
-    //             value: '#ffefee'
-    //         },
-    //         {
-    //             name: '$fu-negative-200',
-    //             className: 'fu-negative-200',
-    //             value: '#ffcbce'
-    //         },
-    //         {
-    //             name: '$fu-negative-300',
-    //             className: 'fu-negative-300',
-    //             value: '#ff9b96'
-    //         },
-    //         {
-    //             name: '$fu-negative-400',
-    //             className: 'fu-negative-400',
-    //             value: '#ff827c'
-    //         },
-    //         {
-    //             name: '$fu-negative-500',
-    //             className: 'fu-negative-500',
-    //             value: '#ff6a63'
-    //         },
-    //         {
-    //             name: '$fu-negative-600',
-    //             className: 'fu-negative-600',
-    //             value: '#ec5d56'
-    //         },
-    //         {
-    //             name: '$fu-negative-700',
-    //             className: 'fu-negative-700',
-    //             value: '#da564f'
-    //         }
-    //     ]
-    // },
-    // {
-    //     name: 'Neutral',
-    //     className: 'fu-neutral',
-    //     colors: [
-    //         {
-    //             name: '$fu-neutral-100',
-    //             className: 'fu-neutral-100',
-    //             value: '#fff8e2'
-    //         },
-    //         {
-    //             name: '$fu-neutral-200',
-    //             className: 'fu-neutral-200',
-    //             value: '#ffe0a3'
-    //         },
-    //         {
-    //             name: '$fu-neutral-500',
-    //             className: 'fu-neutral-500',
-    //             value: '#ffb424'
-    //         },
-    //         {
-    //             name: '$fu-neutral-700',
-    //             className: 'fu-neutral-700',
-    //             value: '#fea222'
-    //         },
-    //         {
-    //             name: '$fu-neutral-900',
-    //             className: 'fu-neutral-900',
-    //             value: '#d8840d'
-    //         }
-    //     ]
-    // }
+    },
+    {
+        name: 'Primary',
+        className: 'fu-primary',
+        value: '#3083FF',
+        colors: [
+            {
+                name: '$fu-primary-100',
+                className: 'fu-primary-100',
+                value: '#E2EEFF'
+            },
+            {
+                name: '$fu-primary-200',
+                className: 'fu-primary-200',
+                value: '#C9DFFF'
+            },
+            {
+                name: '$fu-primary-300',
+                className: 'fu-primary-300',
+                value: '#96C0FF'
+            },
+            {
+                name: '$fu-primary-400',
+                className: 'fu-primary-400',
+                value: '#68A5FF'
+            },
+            {
+                name: '$fu-primary-500',
+                className: 'fu-primary-500',
+                value: '#3083FF'
+            },
+            {
+                name: '$fu-primary-600',
+                className: 'fu-primary-600',
+                value: '#2B76E6'
+            },
+            {
+                name: '$fu-primary-700',
+                className: 'fu-primary-700',
+                value: '#005BE2'
+            },
+            {
+                name: '$fu-primary-800',
+                className: 'fu-primary-800',
+                value: '#0051C9'
+            },
+            {
+                name: '$fu-primary-900',
+                className: 'fu-primary-900',
+                value: '#0046AF'
+            }
+        ]
+    },
+    {
+        name: 'Secondary',
+        className: 'fu-secondary',
+        value: '#00ccd7',
+        colors: [
+            {
+                name: '$fu-secondary-100',
+                className: 'fu-secondary-100',
+                value: '#E1FBFC'
+            },
+            {
+                name: '$fu-secondary-200',
+                className: 'fu-secondary-200',
+                value: '#BFF2F5'
+            },
+            {
+                name: '$fu-secondary-300',
+                className: 'fu-secondary-300',
+                value: '#91E2E7'
+            },
+            {
+                name: '$fu-secondary-500',
+                className: 'fu-secondary-500',
+                value: '#00CCD7'
+            }
+        ]
+    },
+    {
+        name: 'Positive',
+        className: 'fu-positive',
+        value: '#22C891',
+        colors: [
+            {
+                name: '$fu-positive-100',
+                className: 'fu-positive-100',
+                value: '#DAF5EC'
+            },
+            {
+                name: '$fu-positive-200',
+                className: 'fu-positive-200',
+                value: '#B4E9D2'
+            },
+            {
+                name: '$fu-positive-300',
+                className: 'fu-positive-300',
+                value: '#78DDBC'
+            },
+            {
+                name: '$fu-positive-400',
+                className: 'fu-positive-400',
+                value: '#5CD6AD'
+            },
+            {
+                name: '$fu-positive-500',
+                className: 'fu-positive-500',
+                value: '#22C891'
+            },
+            {
+                name: '$fu-positive-600',
+                className: 'fu-positive-600',
+                value: '#1EB281'
+            },
+            {
+                name: '$fu-positive-700',
+                className: 'fu-positive-700',
+                value: '#1B9C71'
+            }
+        ]
+    },
+    {
+        name: 'Neutral',
+        className: 'fu-neutral',
+        value: '#FFB424',
+        colors: [
+            {
+                name: '$fu-neutral-100',
+                className: 'fu-neutral-100',
+                value: '#FFF8E2'
+            },
+            {
+                name: '$fu-neutral-200',
+                className: 'fu-neutral-200',
+                value: '#FFE0A3'
+            },
+            {
+                name: '$fu-neutral-500',
+                className: 'fu-neutral-500',
+                value: '#FFB424'
+            },
+            {
+                name: '$fu-neutral-700',
+                className: 'fu-neutral-700',
+                value: '#FEA222'
+            },
+            {
+                name: '$fu-neutral-900',
+                className: 'fu-neutral-900',
+                value: '#D8840D'
+            }
+        ]
+    },
+    {
+        name: 'Negative',
+        className: 'fu-negative',
+        value: '#FF6A63',
+        colors: [
+            {
+                name: '$fu-negative-100',
+                className: 'fu-negative-100',
+                value: '#FFEFEE'
+            },
+            {
+                name: '$fu-negative-200',
+                className: 'fu-negative-200',
+                value: '#FFCBCE'
+            },
+            {
+                name: '$fu-negative-300',
+                className: 'fu-negative-300',
+                value: '#FF9B96'
+            },
+            {
+                name: '$fu-negative-400',
+                className: 'fu-negative-400',
+                value: '#FF827C'
+            },
+            {
+                name: '$fu-negative-500',
+                className: 'fu-negative-500',
+                value: '#FF6A63'
+            },
+            {
+                name: '$fu-negative-600',
+                className: 'fu-negative-600',
+                value: '#EC5D56'
+            },
+            {
+                name: '$fu-negative-700',
+                className: 'fu-negative-700',
+                value: '#DA564F'
+            }
+        ]
+    },
+    {
+        name: 'Dark',
+        className: 'fu-dark',
+        value: '#0D148C',
+        colors: [
+            {
+                name: ' $fu-dark-100',
+                className: 'fu-dark-100',
+                value: '#CFD0E8'
+            },
+            {
+                name: '$fu-dark-200',
+                className: 'fu-dark-200',
+                value: '#B6B9DD'
+            },
+            {
+                name: '$fu-dark-300',
+                className: 'fu-dark-300',
+                value: '#313D91'
+            },
+            {
+                name: '$fu-dark-400',
+                className: 'fu-dark-400',
+                value: '#212791'
+            },
+            {
+                name: '$fu-dark-500',
+                className: 'fu-dark-500',
+                value: '#0D148C'
+            },
+            {
+                name: '$fu-dark-600',
+                className: 'fu-dark-600',
+                value: '#021074'
+            },
+            {
+                name: '$v2-2dark-700',
+                className: 'fu-dark-700',
+                value: '#1D1C64'
+            },
+            {
+                name: '$v2-2dark-800',
+                className: 'fu-dark-800',
+                value: '#070A46'
+            }
+        ]
+    },
+    {}
 ];
--- projects/fusion-docs/src/app/pages/components/colors-docs-v3/colors-docs-v3.component.ts
+++ projects/fusion-docs/src/app/pages/components/colors-docs-v3/colors-docs-v3.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {THEME_COLORS_PALETTE_V3} from './colors-docs-config-v3';
 import {DocsLayoutService} from '../../docs/docs-layout.service';
+import {isNullOrUndefined} from '@ironsource/fusion-ui';
 
 @Component({
     selector: 'fusion-colors-docs-v2',
@@ -23,5 +24,37 @@ export class ColorsDocsV3Component implements OnInit {
             content: {templateRef: this.colorsFixedContent},
             type: 'fixed'
         });
+    }
+
+    invertColor(hex: string, bw = true): string {
+        if (isNullOrUndefined(hex)) {
+            return hex;
+        }
+        if (hex.startsWith('#')) {
+            hex = hex.slice(1);
+        }
+        // convert 3-digit hex to 6-digits.
+        if (hex.length === 3) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        }
+        if (hex.length !== 6) {
+            throw new Error('Invalid HEX color.');
+        }
+        let r = parseInt(hex.slice(0, 2), 16),
+            g = parseInt(hex.slice(2, 4), 16),
+            b = parseInt(hex.slice(4, 6), 16);
+        if (bw) {
+            // https://stackoverflow.com/a/3943023/112731
+            return r * 0.299 + g * 0.587 + b * 0.114 > 141 //186
+                ? '#1D1C64'
+                : '#FFFFFF';
+        }
+        // pad each with zeros and return
+        return '#' + this.padZero((255 - r).toString(16)) + this.padZero((255 - g).toString(16)) + this.padZero((255 - b).toString(16));
+    }
+
+    private padZero(str: string, len = 2): string {
+        const zeros = new Array(len).join('0');
+        return (zeros + str).slice(-len);
     }
 }
