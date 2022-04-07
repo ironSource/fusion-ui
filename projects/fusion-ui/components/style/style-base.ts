f3236181eb4c4e8cb35f0527cc8f02cbcb7d4c22
AndyKIron <andyk@is.com>
2022-04-07 09:05:02 +0300

    feat(fu-372): refactor StyleBase class to use css variable instead version service

--- projects/fusion-ui/components/style/style-base.ts
+++ projects/fusion-ui/components/style/style-base.ts
@@ -1,8 +1,7 @@
 import {ElementRef, AfterViewInit, Injector, Renderer2, OnDestroy, Directive} from '@angular/core';
-import {filter, takeUntil} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import {VersionService, StyleVersion, FUSION_STYLE_VERSION_PREFIX} from '@ironsource/fusion-ui/services/version';
 import {Observable, Subject} from 'rxjs';
-import {isNullOrUndefined} from '@ironsource/fusion-ui/utils';
 
 /* eslint-disable @angular-eslint/directive-class-suffix */
 @Directive()
@@ -14,33 +13,19 @@ export abstract class StyleBase implements AfterViewInit, OnDestroy {
     styleVersion = StyleVersion;
     selectedVersion$: Observable<StyleVersion> = this.injector.get(VersionService).styleVersion$.pipe(takeUntil(this.onDestroy$));
 
-    private isManualStyle: boolean;
+    uiStyleVersion: string;
 
     ngAfterViewInit() {
-        this.selectedVersion$
-            .pipe(takeUntil(this.onDestroy$), filter(this.isStyleVersionNotManual.bind(this)))
-            .subscribe(this.handleStyleVersion.bind(this));
+        this.handleUIStyleVersion();
     }
 
-    private isStyleVersionNotManual(): boolean {
-        const element = this.injector.get(ElementRef);
-        if (isNullOrUndefined(this.isManualStyle)) {
-            this.isManualStyle = [...element.nativeElement.classList].some(item => item.startsWith(FUSION_STYLE_VERSION_PREFIX));
-        }
-        return !this.isManualStyle;
-    }
-
-    private handleStyleVersion(styleVersion: StyleVersion): void {
+    private handleUIStyleVersion(): void {
         const element = this.injector.get(ElementRef);
         const renderer = this.injector.get(Renderer2);
 
-        element.nativeElement.classList.forEach(item => {
-            if (item.startsWith(FUSION_STYLE_VERSION_PREFIX)) {
-                renderer.removeClass(element.nativeElement, item);
-            }
-        });
+        this.uiStyleVersion = (getComputedStyle(element.nativeElement).getPropertyValue('--fu-style-version') ?? '3').trim();
 
-        renderer.addClass(element.nativeElement, FUSION_STYLE_VERSION_PREFIX + styleVersion.toString());
+        renderer.addClass(element.nativeElement, FUSION_STYLE_VERSION_PREFIX + this.uiStyleVersion);
     }
 
     ngOnDestroy(): void {
