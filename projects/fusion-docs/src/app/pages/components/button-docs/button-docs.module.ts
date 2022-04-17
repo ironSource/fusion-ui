4c739f2bf354703222d3f15427ec3df56e7b7211
Amit <amit.mor@is.com>
2022-04-17 16:28:12 +0300

    chore(fu-378): change component folder structure

--- projects/fusion-docs/src/app/pages/components/button-docs/button-docs.module.ts
+++ projects/fusion-docs/src/app/pages/components/button-docs/button-docs.module.ts
@@ -6,7 +6,7 @@ import {ExampleBlockModule} from '../../../components/example-block/example-bloc
 import {CodeBlockModule} from '../../../components/code-block/code-block.module';
 import {DocsMenuModule} from '../../../components/docs-menu/docs-menu.module';
 import {IconModule, TooltipModule} from '@ironsource/fusion-ui/components';
-import {ButtonModule} from '@ironsource/fusion-ui/components/button';
+import {ButtonModule} from '@ironsource/fusion-ui/components/button/v2';
 
 const routes: Routes = [{path: '', component: ButtonDocsComponent}];
 
--- projects/fusion-ui/components/dropdown-dual-multi-select/dropdown-dual-multi-select-footer/dropdown-dual-multi-select-footer.module.ts
+++ projects/fusion-ui/components/dropdown-dual-multi-select/dropdown-dual-multi-select-footer/dropdown-dual-multi-select-footer.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DropdownDualMultiSelectFooterComponent} from './dropdown-dual-multi-select-footer.component';
-import {ButtonModule} from '@ironsource/fusion-ui/components/button';
+import {ButtonModule} from '@ironsource/fusion-ui/components/button/v2';
 
 @NgModule({
     declarations: [DropdownDualMultiSelectFooterComponent],
