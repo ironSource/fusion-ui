e1c85db76c288326a185acfd3ac0e387f8c44281
chen.levin <chen.levin@ironsrc.com>
2022-04-20 15:36:12 +0300

    chore: stable native bundle

--- projects/fusion-native/src/common/wrapper/wrapper.module.ts
+++ projects/fusion-native/src/common/wrapper/wrapper.module.ts
@@ -3,11 +3,18 @@ import {CommonModule} from '@angular/common';
 import {WrapperComponent} from './wrapper.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {WrapperService} from './wrapper.service';
+import {SVG_OPTIONS_TOKEN} from '@ironsource/fusion-ui/components/svg';
 
 @NgModule({
     declarations: [WrapperComponent],
     exports: [WrapperComponent],
     imports: [CommonModule, ReactiveFormsModule],
-    providers: [WrapperService]
+    providers: [
+        WrapperService,
+        {
+            provide: SVG_OPTIONS_TOKEN,
+            useValue: {assetsPath: 'https://fusion.ironsrc.net/assets'}
+        }
+    ]
 })
 export class WrapperModule {}
