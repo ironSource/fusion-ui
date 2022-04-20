4b38abe83e5b158f5429887e58d8d66d966bdf8f
Amit <amit.mor@is.com>
2022-04-20 10:40:49 +0300

    chore(fu-385): fix bugs

--- projects/fusion-docs/src/app/app.module.ts
+++ projects/fusion-docs/src/app/app.module.ts
@@ -3,18 +3,11 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing';
 import {environment} from '../environments/environment';
-import {ChipFilterModule, IconModule, SvgModule, TooltipModule} from '@ironsource/fusion-ui';
+import {SvgModule, TooltipModule} from '@ironsource/fusion-ui';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SvgModule.forRoot({assetsPath: environment.assetsPath}),
-        TooltipModule,
-        ChipFilterModule,
-        IconModule
-    ],
+    imports: [BrowserModule, AppRoutingModule, SvgModule.forRoot({assetsPath: environment.assetsPath}), TooltipModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
