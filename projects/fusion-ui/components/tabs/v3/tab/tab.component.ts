5bcffea97edbd60d119d2b9ff38a0d1b86b7219d
AndyKIron <andyk@is.com>
2022-05-08 16:04:38 +0300

    fix(fu-394): added tab pre-selected and pre-disabled to native set version to 3.0.0-beta.35

--- projects/fusion-ui/components/tabs/v3/tab/tab.component.ts
+++ projects/fusion-ui/components/tabs/v3/tab/tab.component.ts
@@ -1,8 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Input, Renderer2} from '@angular/core';
 
 @Component({
     selector: 'fusion-tab',
     template: '<div class="fu-tab-content"><ng-content></ng-content></div>',
     styleUrls: ['./tab.component.scss']
 })
-export class TabComponent {}
+export class TabComponent {
+    @Input() set selected(value: boolean) {
+        this.toggleSelected(value);
+    }
+    @Input() set disabled(value: boolean) {
+        this.toggleDisabled(value);
+    }
+
+    constructor(private _element: ElementRef, private _renderer: Renderer2) {}
+
+    private toggleSelected(isSelected: boolean): void {
+        const elementNative = this._element.nativeElement;
+        if (isSelected) {
+            this._renderer.setAttribute(elementNative, 'selected', '');
+        } else {
+            this._renderer.removeAttribute(elementNative, 'selected');
+        }
+    }
+
+    private toggleDisabled(isDisabled: boolean): void {
+        const elementNative = this._element.nativeElement;
+        if (isDisabled) {
+            this._renderer.setAttribute(elementNative, 'disabled', '');
+        } else {
+            this._renderer.removeAttribute(elementNative, 'disabled');
+        }
+    }
+}
