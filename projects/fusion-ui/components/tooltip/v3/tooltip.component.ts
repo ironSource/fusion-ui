bf98d8b38715a682c5284213eaffea45cd888873
Amit <amit.mor@is.com>
2022-05-09 17:06:45 +0300

    chore: release beta version

--- projects/fusion-ui/components/tooltip/v3/tooltip.component.ts
+++ projects/fusion-ui/components/tooltip/v3/tooltip.component.ts
@@ -2,14 +2,24 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'fusion-tooltip',
-    template: `<div [fusionTooltip]="tooltipText">
-        <div><ng-content></ng-content></div>
-        <ng-container *ngIf="!tooltipText">
-            <div *fusionTooltipContent>
-                <ng-content select=".tooltipWithContent"></ng-content>
-            </div>
-        </ng-container>
-    </div>`
+    template: `<div class="tooltip-container">
+        <div [fusionTooltip]="tooltipText">
+            <div><ng-content></ng-content></div>
+            <ng-container *ngIf="!tooltipText">
+                <div *fusionTooltipContent>
+                    <ng-content select=".tooltipWithContent"></ng-content>
+                </div>
+            </ng-container>
+        </div>
+    </div>`,
+    styles: [
+        `
+            .tooltip-container {
+                display: flex;
+                justify-content: center;
+            }
+        `
+    ]
 })
 export class TooltipComponent {
     @Input() set fusionTooltip(value: string) {
