7aca79a830ca387c961080a6f25b3ffba988bdbc
AndyKIron <andyk@is.com>
2022-05-12 11:07:35 +0300

    fix(fu-397): fixed button v3 selector
    Remove attribute selector `[fusion-button]`, button component since V3 will use only "tag" selector

--- projects/fusion-ui/components/button/v3/button.component.ts
+++ projects/fusion-ui/components/button/v3/button.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {ButtonBaseComponent} from '@ironsource/fusion-ui/components/button/common/base';
 
 @Component({
-    selector: 'fusion-button,[fusion-button]',
+    selector: 'fusion-button',
     templateUrl: '../common/base/button.base.component.html',
     styleUrls: ['./button.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
