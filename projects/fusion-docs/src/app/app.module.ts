d52f37d975004dd6b321cbacb4d91662016677a4
Amit <amit.mor@is.com>
2022-05-16 11:58:23 +0300

    chore: fix changes

--- projects/fusion-docs/src/app/app.module.ts
+++ projects/fusion-docs/src/app/app.module.ts
@@ -3,21 +3,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing';
 import {environment} from '../environments/environment';
-import {ChipFilterModule, SvgModule, TooltipModule} from '@ironsource/fusion-ui';
-import {ModalModule} from '@ironsource/fusion-ui/components/modal';
-import {ButtonModule} from '@ironsource/fusion-ui/components/button';
+import {SvgModule} from '@ironsource/fusion-ui';
+import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip';
+import {ChipFilterModule} from '@ironsource/fusion-ui/components/chip-filter';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SvgModule.forRoot({assetsPath: environment.assetsPath}),
-        TooltipModule,
-        ModalModule,
-        ButtonModule,
-        ChipFilterModule
-    ],
+    imports: [BrowserModule, AppRoutingModule, SvgModule.forRoot({assetsPath: environment.assetsPath}), TooltipModule, ChipFilterModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
