c76709cbb270290e773884f93d9b34964f8d3c60
AndyKIron <andyk@is.com>
2022-05-19 09:27:12 +0300

    chore(fu-397): code review changes

--- projects/fusion-ui/components/datepicker/v1/datepicker-header/datepicker-header.component.spec.ts
+++ projects/fusion-ui/components/datepicker/v1/datepicker-header/datepicker-header.component.spec.ts
@@ -1,11 +1,11 @@
 import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DatepickerHeaderComponent} from './datepicker-header.component';
-import {IconModule} from '../../../icon';
-import {InputModule} from '../../../input';
-import {DropdownModule} from '../../../dropdown';
-import {ClickOutsideModule} from '../../../../directives/click-outside';
 import {DatePipe} from '@angular/common';
+import {IconModule} from '@ironsource/fusion-ui/components/icon';
+import {InputModule} from '@ironsource/fusion-ui/components/input';
+import {ClickOutsideModule} from '@ironsource/fusion-ui/directives/click-outside';
+import {DropdownModule} from '@ironsource/fusion-ui/components/dropdown';
 
 describe('DatepickerHeaderComponent', () => {
     let component: DatepickerHeaderComponent;
