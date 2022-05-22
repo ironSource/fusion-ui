2e282018b11ed26f0a809ad2c8e21b19e8592196
Amit <amit.mor@is.com>
2022-05-22 15:29:48 +0300

    chore(FU-407): add unit tests

---
+++ projects/fusion-ui/components/switcher/v3/switcher.component.spec.ts
@@ -0,0 +1,69 @@
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
+import {SwitcherComponent} from './switcher.component';
+import {UniqueIdService} from '@ironsource/fusion-ui/services/unique-id';
+import {FormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
+import {SwitcherItem} from './switcher.entities';
+
+class MockUniqueIdService extends UniqueIdService {
+    getUniqueId() {
+        return parseInt(Math.random().toString().replace('0.', ''), 10);
+    }
+}
+
+const mockOptions: SwitcherItem[] = [
+    {id: 1, title: 'one'},
+    {id: 2, title: 'two'},
+    {id: 3, title: 'three'}
+];
+
+describe('SwitcherComponent', () => {
+    let component: SwitcherComponent;
+    let fixture: ComponentFixture<SwitcherComponent>;
+
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [SwitcherComponent],
+                imports: [FormsModule],
+                providers: [{provide: UniqueIdService, useClass: MockUniqueIdService}]
+            }).compileComponents();
+        })
+    );
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SwitcherComponent);
+        component = fixture.componentInstance;
+
+        component.configuration = {name: 'switcherTest'};
+        component.options = mockOptions;
+
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Must have rendered needed elements', () => {
+        const items = component.options;
+
+        expect(fixture.debugElement.query(By.css('div.fu-switcher-holder'))).toBeTruthy();
+
+        const elDInputs = fixture.debugElement.queryAll(By.css('input[type="radio"]'));
+        const elDLabels = fixture.debugElement.queryAll(By.css('label'));
+
+        expect(elDInputs.length).toBe(mockOptions.length);
+        expect(elDLabels.length).toBe(mockOptions.length);
+
+        elDInputs.forEach((elem, idx) => {
+            expect(elem.nativeElement.getAttribute('id')).toBe(elem.nativeElement.getAttribute('name') + '_' + items[idx].id.toString());
+            expect(elem.nativeElement.getAttribute('name')).toBe('switcherTest');
+        });
+
+        elDLabels.forEach((elem, idx) => {
+            expect(elem.nativeElement.innerText).toBe(mockOptions[idx].title);
+            expect(elem.nativeElement.getAttribute('for')).toBe(component.switcherConfiguration.name + '_' + items[idx].id.toString());
+        });
+    });
+});
