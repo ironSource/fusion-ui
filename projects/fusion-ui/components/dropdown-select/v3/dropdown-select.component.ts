252a1a9ec19806b217aa116245ec3ec77b39cc89
AndyKIron <andyk@is.com>
2022-05-23 11:08:03 +0300

    feat(fu-406): fixes imports in dropdown-select

--- projects/fusion-ui/components/dropdown-select/v3/dropdown-select.component.ts
+++ projects/fusion-ui/components/dropdown-select/v3/dropdown-select.component.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import {DropdownSelectBaseComponent} from '@ironsource/fusion-ui/components/dropdown-select/common/base/dropdown-select.base.component';
+import {DropdownSelectBaseComponent} from '@ironsource/fusion-ui/components/dropdown-select/common/base';
 import {DropdownSearchComponent} from '@ironsource/fusion-ui/components/dropdown-search/v3';
 
 @Component({
