207081a2f7bd1bb18d69b88afb0820b66eadf33a
AndyKIron <andyk@is.com>
2022-05-23 10:49:49 +0300

    feat(fu-406): fix path to dropdown.service

--- projects/fusion-ui/components/dropdown-option/common/base/dropdown-option.base.component.ts
+++ projects/fusion-ui/components/dropdown-option/common/base/dropdown-option.base.component.ts
@@ -1,7 +1,7 @@
 import {Directive, HostBinding, Input, OnInit} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
-import {DropdownService} from '@ironsource/fusion-ui';
 import {DropdownOption} from '@ironsource/fusion-ui/components/dropdown-option/entities';
+import {DropdownService} from '@ironsource/fusion-ui/components/dropdown/servise';
 
 @Directive()
 export abstract class DropdownOptionBaseComponent implements OnInit {
