f8188d4a1d2a1c8502b850bc69377ce83a5c5524
AndyKIron <andyk@is.com>
2022-05-24 16:57:12 +0300

    chore(fu-406): add search to dropdown-search v3

--- projects/fusion-ui/components/dropdown-search/v3/dropdown-search.module.ts
+++ projects/fusion-ui/components/dropdown-search/v3/dropdown-search.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DropdownSearchComponent} from './dropdown-search.component';
-import {InputModule} from '@ironsource/fusion-ui/components/input/v3';
 import {ReactiveFormsModule} from '@angular/forms';
+import {IconModule} from '@ironsource/fusion-ui';
 
 @NgModule({
     declarations: [DropdownSearchComponent],
     exports: [DropdownSearchComponent],
-    imports: [CommonModule, InputModule, ReactiveFormsModule]
+    imports: [CommonModule, IconModule, ReactiveFormsModule]
 })
 export class DropdownSearchModule {}
