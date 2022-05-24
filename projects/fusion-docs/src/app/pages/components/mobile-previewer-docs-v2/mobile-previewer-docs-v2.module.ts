a8ee5e86869ecf48e38b6c671f9aca274eb186fa
Amit <amit.mor@is.com>
2022-05-24 14:56:38 +0300

    chore(FU-409): restructure popup component folders support sub entries

--- projects/fusion-docs/src/app/pages/components/mobile-previewer-docs-v2/mobile-previewer-docs-v2.module.ts
+++ projects/fusion-docs/src/app/pages/components/mobile-previewer-docs-v2/mobile-previewer-docs-v2.module.ts
@@ -5,7 +5,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {ExampleBlockModule} from '../../../components/example-block/example-block.module';
 import {CodeBlockModule} from '../../../components/code-block/code-block.module';
 import {DocsMenuModule} from '../../../components/docs-menu/docs-menu.module';
-import {ButtonModule, MobilePreviewerModule, PopupModule, VideoPlayerModule} from '@ironsource/fusion-ui';
+import {VideoPlayerModule} from '@ironsource/fusion-ui';
+import {MobilePreviewerModule} from '@ironsource/fusion-ui/components/mobile-previewer/v2';
+import {ButtonModule} from '@ironsource/fusion-ui/components/button/v2';
+import {PopupModule} from '@ironsource/fusion-ui/components/popup';
 
 const routes: Routes = [{path: '', component: MobilePreviewerDocsV2Component}];
 
