54e6e76bcc94797629adc7ebfa2e2d41ae7b351b
Amit <amit.mor@is.com>
2022-05-31 20:27:19 +0300

    chore(3.x.x): fix bugs in tooltip

--- projects/fusion-ui/components/input-inline/v2/input-inline.module.ts
+++ projects/fusion-ui/components/input-inline/v2/input-inline.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {InputInlineComponent} from './input-inline.component';
-import {LoaderModule} from '@ironsource/fusion-ui/components/loader';
+import {LoaderModule} from '@ironsource/fusion-ui/components/loader/v2';
 import {IconModule} from '@ironsource/fusion-ui/components/icon';
-import {InputModule} from '@ironsource/fusion-ui/components/input/v1';
-import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip/v1';
+import {InputModule} from '@ironsource/fusion-ui/components/input/v2';
+import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip/v2';
 import {ReactiveFormsModule} from '@angular/forms';
 import {ClickOutsideModule} from '@ironsource/fusion-ui/directives/click-outside';
 
