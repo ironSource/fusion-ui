ed97b1f3e5ef6deab97df1d433bf2341b01474e6
Amit <amit.mor@is.com>
2022-06-13 09:53:11 +0300

    chore(fu-419): fix style

--- projects/fusion-ui/components/dropdown-dual-multi-select/v3/components/dropdown-dual-multi-select-header/dropdown-dual-multi-select-header.module.ts
+++ projects/fusion-ui/components/dropdown-dual-multi-select/v3/components/dropdown-dual-multi-select-header/dropdown-dual-multi-select-header.module.ts
@@ -1,13 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DropdownDualMultiSelectHeaderComponent} from './dropdown-dual-multi-select-header.component';
-import {InputModule} from '@ironsource/fusion-ui/components/input/v2';
 import {ReactiveFormsModule} from '@angular/forms';
 import {IconModule} from '@ironsource/fusion-ui';
 
 @NgModule({
     declarations: [DropdownDualMultiSelectHeaderComponent],
-    imports: [CommonModule, InputModule, ReactiveFormsModule, IconModule],
+    imports: [CommonModule, ReactiveFormsModule, IconModule],
     exports: [DropdownDualMultiSelectHeaderComponent]
 })
 export class DropdownDualMultiSelectHeaderModule {}
