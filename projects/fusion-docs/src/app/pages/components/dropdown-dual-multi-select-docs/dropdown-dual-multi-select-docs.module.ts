d682b93707a25e3e22cf03ccc097f9adb29f3063
Amit <amit.mor@is.com>
2022-06-14 12:40:57 +0300

    chore(fix_design): fix design

--- projects/fusion-docs/src/app/pages/components/dropdown-dual-multi-select-docs/dropdown-dual-multi-select-docs.module.ts
+++ projects/fusion-docs/src/app/pages/components/dropdown-dual-multi-select-docs/dropdown-dual-multi-select-docs.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {DropdownDualMultiSelectDocsComponent} from './dropdown-dual-multi-select-docs.component';
 import {RouterModule, Routes} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
-import {DropdownDualMultiSelectModule} from '@ironsource/fusion-ui/components/dropdown-dual-multi-select/v2';
+import {DropdownDualMultiSelectModule} from '@ironsource/fusion-ui/components/dropdown-dual-multi-select';
 
 const routes: Routes = [{path: '', component: DropdownDualMultiSelectDocsComponent}];
 
