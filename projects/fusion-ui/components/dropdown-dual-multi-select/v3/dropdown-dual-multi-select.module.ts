6f018f6e52fc7ce962af290e2c51ff302e64ce61
Amit <amit.mor@is.com>
2022-06-15 15:01:58 +0300

    chore(fu-420): release beta version

--- projects/fusion-ui/components/dropdown-dual-multi-select/v3/dropdown-dual-multi-select.module.ts
+++ projects/fusion-ui/components/dropdown-dual-multi-select/v3/dropdown-dual-multi-select.module.ts
@@ -6,12 +6,12 @@ import {ClickOutsideModule} from '@ironsource/fusion-ui/directives/click-outside
 import {DynamicComponentsModule} from '@ironsource/fusion-ui/components/dynamic-components/v1';
 import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
 import {ButtonModule} from '@ironsource/fusion-ui/components/button/v3';
+import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip/v3';
 import {CheckboxModule} from '@ironsource/fusion-ui/components/checkbox/v2';
 import {DropdownDualMultiSelectBodyModule} from './components/dropdown-dual-multi-select-body/dropdown-dual-multi-select-body.module';
 import {DropdownDualMultiSelectHeaderModule} from './components/dropdown-dual-multi-select-header/dropdown-dual-multi-select-header.module';
 import {DropdownDualMultiSelectFooterModule} from './components/dropdown-dual-multi-select-footer/dropdown-dual-multi-select-footer.module';
 import {DropdownDualMultiSelectLoadingModule} from './components/dropdown-dual-multi-select-loading/dropdown-dual-multi-select-loading.module';
-import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip/v3';
 
 @NgModule({
     declarations: [DropdownDualMultiSelectComponent],
