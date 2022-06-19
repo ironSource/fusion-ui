91972ff9665b4e2e856ec41b1ce22956ff0e9430
Amit <amit.mor@is.com>
2022-06-19 16:58:53 +0300

    chore(3.x.x): rc version

--- projects/fusion-ui/components/dropdown-dual-multi-select/common/base/dropdown-dual-multi-select.base.component.ts
+++ projects/fusion-ui/components/dropdown-dual-multi-select/common/base/dropdown-dual-multi-select.base.component.ts
@@ -96,7 +96,11 @@ export abstract class DropdownDualMultiSelectBaseComponent extends ApiBase imple
         this.setLabel();
         this.propagateChange(this.preSelectedItems.value);
         this.selectedChange = this.preSelectedItems.value;
-        this.selected$.next(this.placeholder$.getValue());
+        this.selected$.next(
+            this.selectedChange.length === 1
+                ? this.selectedChange[0]?.displayText || this.selectedChange[0]?.title
+                : this.placeholder$.getValue()
+        );
         this.searchControlTerm.setValue('');
         this.viewChange.emit(this.opened$.getValue());
     }
@@ -123,8 +127,9 @@ export abstract class DropdownDualMultiSelectBaseComponent extends ApiBase imple
 
     writeValue(value: DropdownOption[]): void {
         this.preSelectedItems.setValue(value);
+        console.log(this.preSelectedItems.value);
         this.selectedChange = value;
-        this.selected$.next(this.placeholder$.getValue());
+        this.selected$.next(value.length === 1 ? value[0]?.displayText || value[0]?.title : this.placeholder$.getValue());
     }
 
     registerOnChange(fn: any): void {
