e22300c39d9bfa2c2aa5b02a7ad8ef64591d39d0
AndyKIron <andyk@is.com>
2022-06-27 13:55:05 +0300

    chore(2.0.x): debug info added set version 2.0.4-beta.5

--- projects/fusion-ui/components/table/table.service.ts
+++ projects/fusion-ui/components/table/table.service.ts
@@ -116,7 +116,7 @@ export class TableService {
     }
 
     isRowSelected(row: any): boolean {
-        console.log('isRowSelected:', this.selectedRows);
+        console.log('isRowSelected:', this.selectedRows, this.selectedRows.indexOf(row), row);
         return this.selectedRows.indexOf(row) !== -1;
     }
 
