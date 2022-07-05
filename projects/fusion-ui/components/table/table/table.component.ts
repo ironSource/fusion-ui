3010800e0efb62b15dc8279878eb07a060c31510
AndyKIron <andyk@is.com>
2022-07-05 16:50:33 +0300

    chore(2.0.x): debug info added set version 2.0.4-beta.17

--- projects/fusion-ui/components/table/table/table.component.ts
+++ projects/fusion-ui/components/table/table/table.component.ts
@@ -37,6 +37,8 @@ export class TableComponent extends StyleBase implements OnInit, OnChanges, OnDe
     @Input() options: TableOptions = {};
     @Input() columns: TableColumn[] = [];
     @Input() set rows(value: any[] | TableRowsGrouped) {
+        console.log('rows setter >>', value);
+
         this._rows = value ?? [];
         // check for checked
         console.log('setter: ', this._rows);
