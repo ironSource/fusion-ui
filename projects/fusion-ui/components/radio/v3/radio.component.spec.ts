8b4482036058985945ff2dfebe27ed712a0c1ee2
AndyKIron <andyk@is.com>
2022-07-14 15:55:55 +0300

    chore(fu-430): added radiobutton v3 and radio-group v3

---
+++ projects/fusion-ui/components/radio/v3/radio.component.spec.ts
@@ -0,0 +1,27 @@
+import {waitForAsync, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RadioComponent} from './radio.component';
+import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
+
+describe('RadioComponent', () => {
+    let component: RadioComponent;
+    let fixture: ComponentFixture<RadioComponent>;
+
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [RadioComponent],
+                imports: [IconModule]
+            }).compileComponents();
+        })
+    );
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RadioComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
