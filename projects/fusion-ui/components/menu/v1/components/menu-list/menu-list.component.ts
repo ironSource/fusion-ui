0e64acdf6f45d5903f88ecd0dab93229e8ec7923
AndyKIron <andyk@is.com>
2022-09-21 11:18:00 +0300

    chore(3.x.x): remove not needed location (@angular/common) import

--- projects/fusion-ui/components/menu/v1/components/menu-list/menu-list.component.ts
+++ projects/fusion-ui/components/menu/v1/components/menu-list/menu-list.component.ts
@@ -10,7 +10,6 @@ import {
     Output,
     Renderer2
 } from '@angular/core';
-import {Location} from '@angular/common';
 import {isNullOrUndefined} from '@ironsource/fusion-ui/utils';
 import {NavigationEnd, Router} from '@angular/router';
 import {filter, takeUntil, tap} from 'rxjs/operators';
@@ -43,8 +42,7 @@ export class MenuListComponent implements OnDestroy, OnInit {
         private menuService: MenuService,
         @Optional() private router: Router,
         private renderer: Renderer2,
-        private windowService: WindowService,
-        private location: Location
+        private windowService: WindowService
     ) {}
 
     ngOnInit() {
