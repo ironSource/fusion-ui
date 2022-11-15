29c5aea4e4a04b1cf5484f3e8a954844045ba780
AndyKIron <andyk@is.com>
2022-11-15 11:22:16 +0200

    fix(fu-480): play with compodoc

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
     stories: [/*'../stories/!**!/!*.stories.mdx', '../stories/!**!/!*.stories.@(js|jsx|ts|tsx)', */'../projects/fusion-ui/**/*.stories.ts', '../projects/fusion-ui/**/*.stories.mdx'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-designs'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        'storybook-addon-designs'
+    ],
     framework: '@storybook/angular',
     core: {
         builder: '@storybook/builder-webpack5'
