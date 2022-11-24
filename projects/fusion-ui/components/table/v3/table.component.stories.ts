49e9d1335fb60059e47f8eaad956814b85ca9b63
AndyKIron <andyk@is.com>
2022-11-24 11:45:47 +0200

    fix(fu-480-sb-2): default table story done
    - remove sort from columns config

--- projects/fusion-ui/components/table/v3/table.component.stories.ts
+++ projects/fusion-ui/components/table/v3/table.component.stories.ts
@@ -4,21 +4,93 @@ import {CommonModule} from '@angular/common';
 import {environment} from 'stories/environments/environment';
 import {SvgModule} from '@ironsource/fusion-ui/components/svg';
 import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
+import {TableColumn, TableModule, TableOptions} from '@ironsource/fusion-ui/components/table';
 import {TableComponent} from './table.component';
-import {TableColumn, TableColumnTypeEnum, TableOptions} from '@ironsource/fusion-ui/components/table';
-import {InlineInputType} from '@ironsource/fusion-ui/components/input-inline/common/base';
 
 // region Mocking data
-const TABLE_DEFAULT_OPTIONS: TableOptions = {};
-
+const TABLE_DEFAULT_OPTIONS: TableOptions = {
+    sortingType: 'local',
+    noDataSubMessage: 'Try using again with a different filters'
+};
 const TABLE_DEFAULT_COLUMNS_CONFIG: TableColumn[] = [
-    {key: 'live', type: TableColumnTypeEnum.ToggleButton, title: '', width: '45px'},
-    {key: 'name', title: 'Name', sort: 'asc'},
-    {key: 'username', title: 'Username', sort: '', headerAlign: 'right', align: 'right'},
-    {key: 'email', title: 'Email', sort: ''},
+    {key: 'id', title: 'Id'},
+    {key: 'name', title: 'Name'},
+    {key: 'username', title: 'Username'},
+    {key: 'email', title: 'Email'},
     {key: 'website', title: 'Website'}
 ];
-
+const ROWS_DEFAULT_DATA = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        website: 'hildegard.org'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        website: 'anastasia.net'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        website: 'ramiro.info'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        website: 'kale.biz'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        website: 'demarco.info'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        website: 'ola.org'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        website: 'elvis.io'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        website: 'jacynthe.com'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        website: 'conrad.com'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        website: 'ambrose.net'
+    }
+];
 // endregion
 
 export default {
@@ -26,8 +98,8 @@ export default {
     component: TableComponent,
     decorators: [
         moduleMetadata({
-            declarations: [TableComponent],
-            imports: [CommonModule, SvgModule.forRoot({assetsPath: environment.assetsPath}), IconModule]
+            declarations: [],
+            imports: [CommonModule, SvgModule.forRoot({assetsPath: environment.assetsPath}), IconModule, TableModule]
         })
     ],
     parameters: {
@@ -38,14 +110,16 @@ export default {
         }
     },
     args: {
+        options: TABLE_DEFAULT_OPTIONS,
         columns: TABLE_DEFAULT_COLUMNS_CONFIG,
-        rows: []
+        rows: ROWS_DEFAULT_DATA
     }
 } as Meta<TableComponent>;
 
 const TableTemplate: Story<TableComponent> = (args: TableComponent) => ({
     props: {...args},
     template: `<fusion-table
+    [options]="options"
     [columns]="columns"
     [rows]="rows"
 ></fusion-table>`
