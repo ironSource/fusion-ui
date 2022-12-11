9deadb876d2dfb1da661e748112792ca7f2109a2
AndyKIron <andyk@is.com>
2022-12-11 17:47:31 +0200

    chore(fu-480-sb-2/fg-3): check deploy storybook

--- .github/actions/deploy-storybook.sh
+++ .github/actions/deploy-storybook.sh
@@ -4,8 +4,10 @@ set -e
 branch=$1
 folder=$(echo "$branch" | tr / -)
 
+echo Deploy to: $folder
+
 FOLDER=$folder npm run aws:build-storybook && FOLDER=$folder npm run aws:deploy-storybook
 
-FOLDER=$folder npm run cf:invalidate
+#FOLDER=$folder npm run cf:invalidate
 
 exit $lastCode
