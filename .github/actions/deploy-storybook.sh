df1e113c14f52f4bf990313ac1593c399af8241a
AndyKIron <andyk@is.com>
2022-12-12 09:25:53 +0200

    chore(fu-480-sb-2/fg-3): update deploy storybook action with aws credentials

--- .github/actions/deploy-storybook.sh
+++ .github/actions/deploy-storybook.sh
@@ -4,10 +4,8 @@ set -e
 branch=$1
 folder=$(echo "$branch" | tr / -)
 
-echo Deploy to: $folder
-
 npm run aws:build-storybook && FOLDER=$folder npm run aws:deploy-storybook
 
-#FOLDER=$folder npm run cf:invalidate
+FOLDER=$folder npm run cf:invalidate
 
 exit $lastCode
