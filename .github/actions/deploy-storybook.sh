d3bb641007daf09a696de0f08f88850f5c33e8fa
AndyKIron <andyk@is.com>
2023-01-04 11:26:55 +0200

    chore(fg-94): sync GH workflows from 5.x.x (with deps cache)

---
+++ .github/actions/deploy-storybook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+#branch=$1
+#folder=$(echo "$branch" | tr / -)
+
+folder=$1
+
+FOLDER=$folder npm run aws:deploy-storybook
+
+FOLDER=$folder npm run cf:invalidate
+
+exit $lastCode
