9cee459f20b42f451050929b46d64543b49e15a6
AndyKIron <andyk@is.com>
2023-01-05 13:41:45 +0200

    feat(fg-96): add dynamic and static filers for filters-panel stories

--- projects/fusion-ui/components/chip-filters/v3/stories/chip-filters.dynamic.stories.ts
+++ projects/fusion-ui/components/chip-filters/v3/stories/chip-filters.dynamic.stories.ts
@@ -12,7 +12,7 @@ import {DropdownModule} from '@ironsource/fusion-ui/components/dropdown';
 import {DaterangeModule} from '@ironsource/fusion-ui/components/daterange';
 import {DropdownDualMultiSelectModule} from '@ironsource/fusion-ui/components/dropdown-dual-multi-select';
 import {DynamicFilter, DynamicFilterType} from '@ironsource/fusion-ui/components/chip-filters/common/base';
-import {MOCK_COUNTRIES, MOCK_USERS} from '@ironsource/fusion-ui/components/chip-filters/v3/stories/chip-filters.stories.mock';
+import {MOCK_COUNTRIES, MOCK_STATUS, MOCK_USERS} from '@ironsource/fusion-ui/components/chip-filters/v3/stories/chip-filters.stories.mock';
 
 export default {
     title: 'Components/Filters/Filter Panel Dynamic',
@@ -98,5 +98,81 @@ Default.args = {
     ] as DynamicFilter[],
     disableAddFilter: false,
     isSearch: false
+};
+// endregion
+
+// region StaticAndDynamic
+const FilterPaneStaticDynamicDefaultTemplate: Story<ChipFiltersComponent> = (args: ChipFiltersComponent) => ({
+    props: {...args},
+    template: `
+<div style="height: 350px;">
+    <fusion-chip-filters
+        [addFiltersTitle]="addFiltersTitle"
+        [dynamicFilters]="dynamicFilters"
+        [disableAddFilter]="disableAddFilter"
+        [isSearch]="isSearch"
+    >
+        <fusion-chip-filter [configuration]="configChip1">
+            <div class="filter-element">
+                <fusion-dropdown
+                     [placeholderPrefix]="placeholderPrefixChip1"
+                     [placeholder]="placeholderChip1"
+                     [formControl]="fcChip1"
+                     [options]="optionsChip1"
+                     [search]="searchChip1"
+                     [optionsTitle]="optionsTitleChip1"
+                     >
+                </fusion-dropdown>
+            </div>
+        </fusion-chip-filter>
+    </fusion-chip-filters>
+</div>`
+});
+export const StaticAndDynamic = FilterPaneStaticDynamicDefaultTemplate.bind({});
+StaticAndDynamic.args = {
+    addFiltersTitle: 'Add filter by:',
+    dynamicFilters: [
+        {
+            id: 1,
+            title: 'User',
+            filterType: DynamicFilterType.Dropdown,
+            formControl: new FormControl(),
+            dropdownConfiguration: {
+                options: MOCK_USERS,
+                optionsTitle: 'Select User',
+                placeholder: 'All'
+            }
+        },
+        {
+            id: 2,
+            title: 'Countries',
+            filterType: DynamicFilterType.IncludeExclude,
+            formControl: new FormControl(),
+            includeExcludeConfiguration: {
+                items: MOCK_COUNTRIES,
+                placeholder: 'All'
+            }
+        },
+        {
+            id: 3,
+            title: 'Date Range',
+            filterType: DynamicFilterType.DateRange,
+            formControl: new FormControl(),
+            daterangeConfiguration: {
+                options: {chipLabel: 'Date range', placeholder: 'All', overlayAlignPosition: 'left'}
+            }
+        }
+    ] as DynamicFilter[],
+    disableAddFilter: false,
+    isSearch: false,
+
+    // region first chip - User
+    configChip1: {id: 0, mode: 'static', close: true},
+    fcChip1: new FormControl(),
+    placeholderPrefixChip1: 'Status',
+    placeholderChip1: 'All',
+    optionsChip1: MOCK_STATUS,
+    optionsTitleChip1: 'Status'
+    // endregion
 };
 // endregion
