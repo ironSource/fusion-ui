528d26c3b2e783e0773665334c5853343fef4537
AndyKIron <andyk@is.com>
2023-01-15 15:05:52 +0200

    chore(fg-106): done with v3 fusion-search component with stories

---
+++ projects/fusion-ui/components/search/v3/search.component.stories.ts
@@ -0,0 +1,135 @@
+import {Story, Meta} from '@storybook/angular';
+import {moduleMetadata} from '@storybook/angular';
+import {CommonModule} from '@angular/common';
+import {dedent} from 'ts-dedent';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SvgModule} from '@ironsource/fusion-ui/components/svg';
+import {environment} from '../../../../../stories/environments/environment';
+
+import {SearchComponent} from '@ironsource/fusion-ui/components/search';
+
+const formControl = new FormControl();
+const formControlWithValue = new FormControl('Typing something');
+const formControlDisabled = new FormControl({value: 'Typing something', disabled: true});
+
+export default {
+    title: 'Components/Search',
+    component: SearchComponent,
+    decorators: [
+        moduleMetadata({
+            declarations: [],
+            imports: [CommonModule, SvgModule.forRoot({assetsPath: environment.assetsPath}), FormsModule, ReactiveFormsModule]
+        })
+    ],
+    parameters: {
+        docs: {
+            description: {
+                component: dedent`**SearchComponent component**`
+            }
+        },
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/V4eZU3qDgKYPhR4eaTvSwy/%F0%9F%8E%A8-Style-guide-2021-Master?node-id=5204%3A224368&t=W94xASouNL6qlRXv-1'
+        },
+        backgrounds: {
+            default: 'grey',
+            values: [
+                {
+                    name: 'white',
+                    value: '#FFFFFF'
+                },
+                {
+                    name: 'grey',
+                    value: '#F5F5F5'
+                }
+            ]
+        }
+    },
+    argTypes: {
+        formControl: {
+            control: false
+        },
+        placeholder: {
+            control: 'text'
+        },
+        searchClassName: {
+            control: false
+        },
+        transparentClassName: {
+            control: false
+        }
+    }
+} as Meta<SearchComponent>;
+
+const TooltipTemplate: Story<SearchComponent> = (args: SearchComponent) => ({
+    props: {...args},
+    template: `<div style="width: 230px;"><fusion-search class="{{searchClassName}} {{transparentClassName}}" [showClearIcon]="showClearIcon" [placeholder]="placeholder" [formControl]="formControl"></fusion-search></div>`
+});
+
+// region Default
+export const Default = TooltipTemplate.bind({});
+Default.args = {
+    formControl: formControl
+};
+// endregion
+
+// region WithClearIcon
+export const WithClearIcon = TooltipTemplate.bind({});
+WithClearIcon.args = {
+    formControl: formControl,
+    showClearIcon: true
+};
+// endregion
+
+// region Small
+export const Small = TooltipTemplate.bind({});
+Small.args = {
+    formControl: formControl,
+    searchClassName: 'fu-small'
+};
+// endregion
+
+// region SmallWithClearIcon
+export const SmallWithClearIcon = TooltipTemplate.bind({});
+SmallWithClearIcon.args = {
+    formControl: formControl,
+    searchClassName: 'fu-small',
+    showClearIcon: true
+};
+// endregion
+
+// region Transparent
+export const Transparent = TooltipTemplate.bind({});
+Transparent.args = {
+    formControl: formControl,
+    transparentClassName: 'fu-transparent'
+};
+// endregion
+
+// region TransparentWithClearIcon
+export const TransparentWithClearIcon = TooltipTemplate.bind({});
+TransparentWithClearIcon.args = {
+    formControl: formControl,
+    transparentClassName: 'fu-transparent',
+    showClearIcon: true
+};
+// endregion
+
+// region TransparentSmallWithClearIcon
+export const TransparentSmallWithClearIcon = TooltipTemplate.bind({});
+TransparentSmallWithClearIcon.args = {
+    formControl: formControl,
+    searchClassName: 'fu-small',
+    transparentClassName: 'fu-transparent',
+    showClearIcon: true
+};
+// endregion
+
+// region Disabled - wait for design for Shai
+/*
+export const Disabled = TooltipTemplate.bind({});
+Disabled.args = {
+    formControl: formControlDisabled
+};
+*/
+// endregion
--- projects/fusion-ui/components/search/v3/search.component.ts
+++ projects/fusion-ui/components/search/v3/search.component.ts
@@ -1,15 +1,135 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, forwardRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormControl, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {isNullOrUndefined} from '@ironsource/fusion-ui';
+import {IconData, IconModule} from '@ironsource/fusion-ui/components/icon/v1';
 
 @Component({
     selector: 'fusion-search',
-    standalone: true,
-    imports: [CommonModule],
     templateUrl: './search.component.html',
-    styleUrls: ['./search.component.scss']
+    styleUrls: ['./search.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, IconModule],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => SearchComponent),
+            multi: true
+        }
+    ]
 })
-export class SearchComponent implements OnInit {
-    constructor() {}
+export class SearchComponent implements OnInit, OnDestroy {
+    /** @internal */
+    @ViewChild('input') inputElRef: ElementRef;
 
-    ngOnInit(): void {}
+    private _placeholder = 'Search';
+    @Input()
+    set placeholder(value: string) {
+        if (!isNullOrUndefined(value)) {
+            this._placeholder = value;
+        }
+    }
+    get placeholder(): string {
+        return this._placeholder;
+    }
+
+    private _showClearIcon = true;
+    @Input()
+    set showClearIcon(value: boolean) {
+        this._showClearIcon = value;
+    }
+    get showClearIcon(): boolean {
+        return this._showClearIcon;
+    }
+
+    /** @internal */
+    searchFormControl = new FormControl();
+    /** @internal */
+    onDestroy$ = new Subject<void>();
+    /** @internal */
+    searchIcon: IconData = {iconName: 'search-bold', iconVersion: 'v3'};
+    /** @internal */
+    clearIcon: IconData = {iconName: 'cancel', iconVersion: 'v3'};
+
+    ngOnInit() {
+        this.searchFormControl.valueChanges.pipe(takeUntil(this.onDestroy$)).subscribe(value => {
+            this.propagateTouched();
+            this.propagateChange(value);
+        });
+    }
+
+    ngOnDestroy() {
+        this.onDestroy$.next();
+        this.onDestroy$.complete();
+    }
+
+    // region Implement ControlValueAccessor methods
+    /**
+     * Method to call when value has changes.
+     * @ignore
+     */
+    propagateChange = (_: boolean) => {};
+
+    /**
+     * Method to call when the component is touched (when it was is clicked).
+     * @ignore
+     */
+    propagateTouched = () => {};
+
+    /**
+     * update value from model to the component
+     * @ignore
+     */
+    writeValue(value: string): void {
+        if (isNullOrUndefined(value)) {
+            this.clearInput();
+        } else {
+            this.searchFormControl.setValue(value, {emitEvent: false});
+        }
+    }
+
+    /**
+     * Informs the outside world about changes.
+     * see method propagateChange call - this.propagateChange(this.model);
+     * @ignore
+     */
+    registerOnChange(fn: any): void {
+        this.propagateChange = fn;
+    }
+
+    /**
+     * on click
+     * @ignore
+     */
+    registerOnTouched(fn: any): void {
+        this.propagateTouched = fn;
+    }
+
+    /**
+     * on set form controll enabled / disabled
+     * also do UI Component enabled / disabled
+     * @ignore
+     */
+    setDisabledState?(disabled: boolean): void {
+        if (disabled) {
+            this.searchFormControl.disable();
+        } else {
+            this.searchFormControl.enable();
+        }
+    }
+    // endregion
+
+    private clearInput(withEvent = false): void {
+        this.searchFormControl.setValue('', {emitEvent: withEvent});
+        if (withEvent) {
+            this.setFocus();
+        }
+    }
+
+    private setFocus(): void {
+        this.inputElRef.nativeElement.focus();
+    }
 }
