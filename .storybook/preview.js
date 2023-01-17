eb72b81d3dd18c198ac44410e39179d141ca4bbc
AndyKIron <andyk@is.com>
2023-01-17 15:25:47 +0200

    chore(5.x.x): update fusion-ui to 5.1.0-rc.15 for stackblitz in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,7 +13,7 @@ export const parameters = {
     },
     docs: {inlineStories: true},
     stackblitzAdditionalDependency: {
-        "@ironsource/fusion-ui": '5.1.0-rc.14'
+        "@ironsource/fusion-ui": '5.1.0-rc.15'
     },
     options: {
         storySort: (a, b) => {
