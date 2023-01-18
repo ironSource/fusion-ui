fcd6805c346b1365023ead5b0a669c2bedb79b1b
AndyKIron <andyk@is.com>
2023-01-18 17:49:26 +0200

    chore(fg-107): fixed icon only button in table cell - float action trigger

--- projects/fusion-ui/components/button/common/base/button.base.component.ts
+++ projects/fusion-ui/components/button/common/base/button.base.component.ts
@@ -88,7 +88,7 @@ export abstract class ButtonBaseComponent implements OnInit, AfterViewInit, OnDe
     }
 
     ngAfterViewInit() {
-        this.projectContent = !!this.element.nativeElement.innerText;
+        this.projectContent = !!this.element.nativeElement.innerText.trim();
         this.setHostClass(this.projectContent, 'fu-with-content');
     }
 
