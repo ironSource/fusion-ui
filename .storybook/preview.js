c5a04dd96d714363759dd0e8142e3b4ec359f1d0
AndyKIron <andyk@is.com>
2023-01-18 18:09:22 +0200

    chore(fg-107): set version for stackblitz

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,7 +13,7 @@ export const parameters = {
     },
     docs: {inlineStories: true},
     stackblitzAdditionalDependency: {
-        "@ironsource/fusion-ui": '5.1.0-beta.2'
+        "@ironsource/fusion-ui": '5.1.0-rc.19'
     },
     options: {
         storySort: (a, b) => {
