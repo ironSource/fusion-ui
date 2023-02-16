a026d15e288fd1c60080cc66139eb19a72428aee
AndyKIron <andyk@is.com>
2023-02-16 12:17:57 +0200

    chore(fg-118): added '%' type to the input-inline component

--- projects/fusion-ui/components/input-inline/v3/input-inline.component.ts
+++ projects/fusion-ui/components/input-inline/v3/input-inline.component.ts
@@ -1,14 +1,10 @@
 import {ChangeDetectionStrategy, Component, forwardRef, Input, OnInit} from '@angular/core';
 import {NG_VALUE_ACCESSOR, ReactiveFormsModule} from '@angular/forms';
-import {
-    CONFIG_INPUT_INLINE_BY_UI_STYLE,
-    InputInlineBaseComponent,
-    InputInlineConfigByStyle
-} from '@ironsource/fusion-ui/components/input-inline/common/base';
+import {CONFIG_INPUT_INLINE_BY_UI_STYLE, InputInlineBaseComponent} from '@ironsource/fusion-ui/components/input-inline/common/base';
 import {CommonModule} from '@angular/common';
 import {LoaderModule} from '@ironsource/fusion-ui/components/loader/v2';
 import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
-import {InputModule, InputOptions, InputSize} from '@ironsource/fusion-ui/components/input/v3';
+import {InputModule} from '@ironsource/fusion-ui/components/input/v3';
 import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip/v3';
 import {ClickOutsideModule} from '@ironsource/fusion-ui/directives/click-outside';
 
