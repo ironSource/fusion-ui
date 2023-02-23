c1ee51d62cc28bc35a871f2a239f197c1a968661
AndyKIron <andyk@is.com>
2023-02-23 11:45:56 +0200

    chore(5.x.x): added story example for tooltip with link (prevent to close)

--- projects/fusion-ui/components/tooltip/v3/tooltip.implementation.stories.ts
+++ projects/fusion-ui/components/tooltip/v3/tooltip.implementation.stories.ts
@@ -126,6 +126,67 @@ export class FusionStoryWrapperComponent {
     }
 };
 
+const TooltipInlineWithLinkTemplate: Story<TooltipComponent> = (args: TooltipComponent) => ({
+    props: args,
+    template: `
+<fusion-tooltip [tooltipConfiguration]="tooltipConfiguration">
+    <div class="fusionTooltipTrigger">
+        <div>Element with tooltip</div>
+    </div>
+    <div class="fusionTooltipContent">
+        <div style="margin-bottom: 5px; font-size: 14px">Tooltip title</div>
+        <div>{{fusionTooltip}} <a href="https://fusion.ironsrc.net/" target="_blank" style="color: #ffffff">click me</a></div>
+    </div>
+</fusion-tooltip>
+`
+});
+export const InlinePreventToClose = TooltipInlineWithLinkTemplate.bind({});
+InlinePreventToClose.args = {
+    tooltipConfiguration: {
+        preventTooltipToClose: true
+    }
+};
+InlinePreventToClose.parameters = {
+    docs: {
+        description: {
+            story: dedent`**Tooltip component** with link in tooltip content
+
+            In this case need "prevent" hide tooltip on "tooltiped" element "mouseleave" event.
+            For this need to add to the tooltipConfiguration \`preventTooltipToClose: true\`
+            `
+        },
+        source: {
+            language: 'typescript',
+            code: dedent`
+import { Component } from '@angular/core';
+import { TooltipModule } from '@ironsource/fusion-ui/components/tooltip/v3';
+
+@Component({
+  selector: 'fusion-story-wrapper',
+  template: \`<div style="width:150px; margin:auto; margin-top: 100px">
+  <fusion-tooltip [tooltipConfiguration]="{preventTooltipToClose: true}">
+    <div class="fusionTooltipTrigger">
+        <div>Element with tooltip</div>
+    </div>
+    <div class="fusionTooltipContent">
+        <div style="margin-bottom: 5px; font-size: 14px">Tooltip title</div>
+        <div>Well done! You successfully read this alert message <a href="https://fusion.ironsrc.net/" target="_blank" style="color: #ffffff">click me</a></div>
+    </div>
+</fusion-tooltip>
+</div>\`,
+  standalone: true,
+  imports: [TooltipModule],
+})
+export class FusionStoryWrapperComponent {
+  tooltipText = 'Well done! You successfully read this alert message';
+}
+`,
+            format: true,
+            type: 'code'
+        }
+    }
+};
+
 export const InlineCustomization = TooltipInlineTemplate.bind({});
 InlineCustomization.args = {
     tooltipConfiguration: {
