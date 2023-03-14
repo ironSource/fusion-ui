4c30df9ed4681e63bfdf94e114934c9a3d0ea9bf
AndyKIron <andyk@is.com>
2023-03-14 15:55:31 +0200

    chore(fg-131): fix for storybook input stringify

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,22 @@
 import {setCompodocJson} from '@storybook/addon-docs/angular';
 import docJson from '../documentation.json';
 
+// region fix input stringify
+// see https://discord.com/channels/748677963142135818/1054398047863455795/1054398047863455795
+// see https://github.com/storybookjs/storybook/issues/17004
+const allFields = docJson.components
+    ?.map(c => ([...(c.propertiesClass || []), ...(c.inputsClass || [])]))
+    .flat();
+
+allFields.forEach(p => {
+    if (p.type === 'number') {
+        p.defaultValue = +(p.defaultValue);
+    } else {
+        delete p.defaultValue;
+    }
+});
+// endregion
+
 setCompodocJson(docJson);
 
 export const parameters = {
