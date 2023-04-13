190bcb256ce32699ab50ed045323ea70a0cb8655
AndyKIron <andyk@is.com>
2023-04-13 17:53:19 +0300

    chore(fg-131): fixed click outside for user popup menu

--- projects/fusion-ui/components/navigation-menu/v4/navigation-pop-menu/navigation-pop-menu.component.ts
+++ projects/fusion-ui/components/navigation-menu/v4/navigation-pop-menu/navigation-pop-menu.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IconData, IconModule} from '@ironsource/fusion-ui/components/icon/v1';
 import {LayoutUser} from '@ironsource/fusion-ui/entities';
@@ -12,7 +12,7 @@ import {MenuItem} from '@ironsource/fusion-ui/components/menu/common/base';
     styleUrls: ['./navigation-pop-menu.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavigationPopMenuComponent implements OnInit {
+export class NavigationPopMenuComponent implements OnInit, AfterViewInit {
     @Input() layoutUser: LayoutUser;
     @Input() menuItems: MenuItem[];
 
@@ -21,7 +21,12 @@ export class NavigationPopMenuComponent implements OnInit {
     /** @internal */
     userIcon: IconData = {iconName: 'userCircle', iconVersion: 'v4'};
 
-    constructor() {}
+    constructor(private element: ElementRef) {}
 
     ngOnInit(): void {}
+
+    ngAfterViewInit() {
+        const popupHeight = this.element.nativeElement.querySelector('.fu-pop-menu')?.offsetHeight;
+        this.element.nativeElement.style.height = `${popupHeight}px`;
+    }
 }
