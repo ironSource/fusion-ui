92c8bc122175a7a74d0ff9258abc08b32cdd0505
AndyKIron <andyk@is.com>
2023-04-30 12:11:13 +0300

    chore(fg-131): added inner content - example

--- projects/fusion-ui/components/layout/v4/stories/layout-story-wrapper.component.ts
+++ projects/fusion-ui/components/layout/v4/stories/layout-story-wrapper.component.ts
@@ -17,8 +17,35 @@ import {takeUntil} from 'rxjs/operators';
             [headerContent]="headerContent"
             (menuItemClick)="onMenuItemClick($event)"
             (pageBackButtonClicked)="onPageBackButtonClicked($event)"
-        ></fusion-layout>
+        >
+            <div class="parent">
+                <div class="div1">1</div>
+                <div class="div2">2</div>
+                <div class="div3">3</div>
+                <div class="div4">4</div>
+                <div class="div5">5</div>
+                <div class="div6">6</div>
+                <div class="div7">7</div>
+                <div class="div8">8</div>
+                <div class="div9">9</div>
+                <div class="div10">10</div>
+            </div>
+        </fusion-layout>
     `,
+    styles: [
+        '.parent {padding: 24px; width: 100%; height: 100%; display: grid; grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(4, 1fr); grid-column-gap: 16px; grid-row-gap: 24px;}',
+        '.div1 { grid-area: 1 / 1 / 2 / 2; }',
+        '.div2 { grid-area: 1 / 2 / 2 / 3; }',
+        '.div3 { grid-area: 1 / 3 / 2 / 4; }',
+        '.div4 { grid-area: 1 / 4 / 2 / 5; }',
+        '.div5 { grid-area: 2 / 1 / 4 / 3; }',
+        '.div6 { grid-area: 2 / 3 / 4 / 5; }',
+        '.div7 { grid-area: 4 / 1 / 5 / 2; }',
+        '.div8 { grid-area: 4 / 2 / 5 / 3; }',
+        '.div9 { grid-area: 4 / 3 / 5 / 4; }',
+        '.div10 { grid-area: 4 / 4 / 5 / 5; }',
+        '.parent div{background: #F7F7F7; border: 1px solid rgba(0, 0, 0, 0.08); border-radius: 4px; display: flex; align-items: center; justify-content: center}'
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutStoryWrapperComponent implements OnInit, OnDestroy {
