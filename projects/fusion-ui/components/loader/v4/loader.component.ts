de055b156fa85e20832300a48d8ba649ee8311c4
AndyKIron <andy.kononenko@unity3d.com>
2023-12-17 12:30:31 +0200

    chore(isct-137): code-review changes

--- projects/fusion-ui/components/loader/v4/loader.component.ts
+++ projects/fusion-ui/components/loader/v4/loader.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {defaultStrokeWidth} from './loader.configuration';
 
 @Component({
     selector: 'fusion-loader',
@@ -15,7 +16,7 @@ export class LoaderComponent {
     }
 
     get strokeWidth(): number {
-        return this._strokeWidth ?? 2;
+        return this._strokeWidth ?? defaultStrokeWidth;
     }
 
     private _strokeWidth: number;
