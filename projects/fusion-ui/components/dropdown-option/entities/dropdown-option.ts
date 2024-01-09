64aabed6013d1db75d6b00d479a5a2f5876e24e6
AndyKIron <andy.kononenko@unity3d.com>
2024-01-09 16:57:49 +0200

    chore(isct-145): dropdown option component v4

--- projects/fusion-ui/components/dropdown-option/entities/dropdown-option.ts
+++ projects/fusion-ui/components/dropdown-option/entities/dropdown-option.ts
@@ -1,9 +1,10 @@
 import {DynamicComponentConfiguration} from '@ironsource/fusion-ui/components/dynamic-components/common/entities';
 import {IconData} from '@ironsource/fusion-ui/components/icon/v1';
+import {CountryCode} from '@ironsource/fusion-ui/components/flag/v4';
 
 export interface DropdownOption {
     icon?: IconData;
-    flag?: string;
+    flag?: string | CountryCode;
     title?: string;
     displayText?: string;
     titleText?: string;
---
+++ projects/fusion-ui/components/dropdown-option/v4/dropdown-option-v4.component.stories.ts
@@ -0,0 +1,133 @@
+import {Meta, StoryObj, moduleMetadata, componentWrapperDecorator} from '@storybook/angular';
+import {dedent} from 'ts-dedent';
+import {CommonModule} from '@angular/common';
+import {environment} from 'stories/environments/environment';
+import {SvgModule} from '@ironsource/fusion-ui/components/svg';
+import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
+import {DropdownOptionV4Component} from './dropdown-option-v4.component';
+import {DropdownOption} from '@ironsource/fusion-ui/components/dropdown-option';
+
+export default {
+    title: 'V4/Components/Dropdown/Option',
+    component: DropdownOptionV4Component,
+    decorators: [
+        moduleMetadata({
+            declarations: [],
+            imports: [CommonModule, SvgModule.forRoot({assetsPath: environment.assetsPath}), IconModule]
+        }),
+        componentWrapperDecorator(story => `<div style="width: 200px;">${story}</div>`)
+    ],
+    tags: ['autodocs'],
+    parameters: {
+        docs: {
+            description: {
+                component: dedent`***SearchComponent***.
+                `
+            }
+        },
+        options: {
+            showPanel: true,
+            panelPosition: 'bottom'
+        }
+    },
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            isDisabled: false
+        } as DropdownOption
+    }
+} as Meta<DropdownOptionV4Component>;
+
+type Story = StoryObj<DropdownOptionV4Component>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            isDisabled: true
+        } as DropdownOption
+    }
+};
+
+export const Icon: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            icon: 'ph/placeholder',
+            isDisabled: false
+        } as DropdownOption
+    }
+};
+
+export const AppAndIcon: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            image: 'https://platform.ssacdn.com/demand-creatives/icons/icon_43609a8611f49ebe8c8e5eeef199112b_51b1115a8ee50c907e46d380d88ce4a9.jpeg',
+            icon: 'v4/branded/android',
+            isDisabled: false
+        } as DropdownOption
+    }
+};
+
+export const App: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            image: 'https://platform.ssacdn.com/demand-creatives/icons/icon_43609a8611f49ebe8c8e5eeef199112b_51b1115a8ee50c907e46d380d88ce4a9.jpeg',
+            isDisabled: false
+        } as DropdownOption
+    }
+};
+
+export const Brand: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            icon: 'v4/branded/android',
+            isDisabled: false
+        } as DropdownOption
+    }
+};
+
+export const Flag: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            flag: 'us',
+            isDisabled: false
+        } as DropdownOption
+    }
+};
+
+export const WithAppIconAndCaption: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Label',
+            image: 'https://platform.ssacdn.com/demand-creatives/icons/icon_43609a8611f49ebe8c8e5eeef199112b_51b1115a8ee50c907e46d380d88ce4a9.jpeg',
+            icon: 'v4/branded/android',
+            subText: {text: 'Caption'},
+            isDisabled: false
+        } as DropdownOption
+    }
+};
+
+export const WithLongText: Story = {
+    args: {
+        option: {
+            id: '1',
+            displayText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.    ',
+            isDisabled: false
+        } as DropdownOption
+    }
+};
