0e263a8a02d766d795d3bc0c1f3af58844b55922
Amit <amit.mor@is.com>
2024-01-30 09:11:37 +0200

    feat(ISCT-250): add sortable item list drag and drop directive

---
+++ projects/fusion-ui/directives/drag-and-drop-sortable/drag-and-drop-sortable.directive.ts
@@ -0,0 +1,85 @@
+import {ContentChildren, Directive, ElementRef, EventEmitter, HostListener, Input, Output, QueryList} from '@angular/core';
+import {DragAndDropSortableService} from './drag-and-drop-sortable.service';
+
+@Directive({
+    selector: '[isDragAndDropSortable]',
+    standalone: true,
+    providers: [DragAndDropSortableService]
+})
+export class DragAndDropSortableDirective {
+    @ContentChildren('listItem') set draggableListItems(value: QueryList<ElementRef>) {
+        this.listItems = value;
+        this.setDraggableAttributeToItems(this.listItems);
+    }
+
+    @Input() sortableModal: any[];
+
+    @Output() sortableModalChange = new EventEmitter<any[]>();
+    @Output() onDragElementDrop = new EventEmitter<any>();
+    @Output() onDragElementEnd = new EventEmitter<any>();
+    @Output() onDragElementStart = new EventEmitter<any>();
+
+    private dragElement: HTMLElement;
+    private listItems: QueryList<ElementRef>;
+
+    constructor(private hostElement: ElementRef, private readonly dragAndDropStableService: DragAndDropSortableService) {}
+
+    @HostListener('dragstart', ['$event'])
+    onDragStart($event: any) {
+        $event.stopPropagation();
+        const draggableElement = $event?.target?.closest('[draggable]');
+        if (draggableElement) {
+            this.dragElement = draggableElement;
+            this.dragElement.classList.add('dragging');
+            this.onDragElementStart.emit(this.dragElement);
+        }
+    }
+
+    @HostListener('dragend', ['$event'])
+    onDragEnd($event: any) {
+        const draggableElement = $event?.target?.closest('[draggable]');
+        if (draggableElement) {
+            this.dragElement.classList.remove('dragging');
+            this.dragElement.classList.remove('dragging-transit');
+            this.onDragElementEnd.emit();
+        }
+    }
+
+    @HostListener('dragover', ['$event'])
+    onDragOver($event: any) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        this.dragElement.classList.add('dragging-transit');
+        const afterElement = this.dragAndDropStableService.getDragAfterElement($event.clientY, this.hostElement);
+        if (!afterElement) {
+            this.hostElement.nativeElement.appendChild(this.dragElement);
+        } else {
+            this.hostElement.nativeElement.insertBefore(this.dragElement, afterElement);
+        }
+        this.dragAndDropStableService.onDragToEdgeOfScrollableContainer({
+            dragElement: this.dragElement,
+            containerElement: this.hostElement.nativeElement
+        });
+        this.sortableModal = [...this.hostElement.nativeElement.children].map(childEl => childEl.id);
+    }
+
+    @HostListener('drop', ['$event'])
+    onDrop($event: any) {
+        const draggableElement = $event?.target?.closest('[draggable]');
+        if (draggableElement) {
+            this.dragElement.classList.remove('dragging');
+            this.dragElement.classList.remove('dragging-transit');
+            this.sortableModalChange.emit(this.sortableModal);
+            this.onDragElementDrop.emit(this.dragElement);
+        }
+    }
+
+    private setDraggableAttributeToItems(listItems: QueryList<ElementRef>) {
+        listItems.forEach(e => {
+            const isDraggable = e.nativeElement.getAttribute('draggable');
+            if (!isDraggable) {
+                e.nativeElement.setAttribute('draggable', true);
+            }
+        });
+    }
+}
---
+++ projects/fusion-ui/directives/drag-and-drop-sortable/drag-and-drop-sortable.service.ts
@@ -0,0 +1,41 @@
+import {ElementRef, Injectable} from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DragAndDropSortableService {
+    getDragAfterElement(y: number, hostElement: ElementRef): HTMLElement {
+        const draggableElements = [...hostElement.nativeElement.children].filter(child => !child.classList.contains('dragging'));
+        return draggableElements.reduce(
+            (closest, child) => {
+                const box = child.getBoundingClientRect();
+                const offset = y - box.top - box.height / 2;
+                return offset < 0 && offset > closest.offset ? {offset, element: child} : closest;
+            },
+            {offset: Number.NEGATIVE_INFINITY}
+        ).element;
+    }
+
+    onDragToEdgeOfScrollableContainer({dragElement, containerElement}: {dragElement: HTMLElement; containerElement: HTMLElement}) {
+        let pageY = 0;
+        let distanceFromTopOfContainer = 0;
+        if (dragElement) {
+            pageY = dragElement.getBoundingClientRect().top;
+            distanceFromTopOfContainer = pageY - containerElement.getBoundingClientRect().top;
+
+            if (distanceFromTopOfContainer < 30) {
+                this.scrollEntities({containerElement, action: 'scrollUp'});
+            } else if (distanceFromTopOfContainer > containerElement.clientHeight - 30) {
+                this.scrollEntities({containerElement, action: 'scrollDown'});
+            }
+        }
+    }
+
+    private scrollEntities({containerElement, action}: {containerElement: HTMLElement; action: string}) {
+        if (action === 'scrollDown') {
+            containerElement.scrollTop += 10;
+        } else if (action === 'scrollUp') {
+            containerElement.scrollTop -= 10;
+        }
+    }
+}
