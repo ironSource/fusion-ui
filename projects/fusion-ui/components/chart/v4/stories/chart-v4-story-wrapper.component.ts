f67cdaaae566b1ae0e73c3da2b757570ba67acac
AndyKIron <andy.kononenko@unity3d.com>
2024-02-11 15:05:13 +0200

    chore(isct-197): fix legends shown on story wrapper

--- projects/fusion-ui/components/chart/v4/stories/chart-v4-story-wrapper.component.ts
+++ projects/fusion-ui/components/chart/v4/stories/chart-v4-story-wrapper.component.ts
@@ -20,7 +20,7 @@ import {ChartV4Component} from '../chart-v4.component';
                 (afterDatasetInit)="onChartInit($event)"
             ></fusion-chart>
         </div>
-        <div class="fusion-chart-labels-wrapper" *ngIf="data && (chartDataLabels$ | async).length">
+        <div class="fusion-chart-labels-wrapper" *ngIf="data && type !== 'doughnut'">
             <fusion-chart-labels [labels]="chartDataLabels$ | async" (labelHover)="labelHovered($event)"></fusion-chart-labels>
         </div>
         <div *ngIf="!data" class="fu-empty-state">
