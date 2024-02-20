4767058f766388f8e8246c905b25159b1bf9079c
AndyKIron <andy.kononenko@unity3d.com>
2024-02-20 11:38:12 +0200

    chore(isct-197): update chart stacked line fill opacity to 0.8

--- projects/fusion-ui/components/chart/common/base/chart.config.ts
+++ projects/fusion-ui/components/chart/common/base/chart.config.ts
@@ -44,7 +44,7 @@ export const BASE_DATASET_OPTIONS: {[key: string]: ChartBaseDatasetOptions} = {
     },
     style_v4: {
         colorSettings: ['backgroundColor', 'borderColor', 'pointBorderColor', 'pointBackgroundColor'],
-        fillOpacity: 70,
+        fillOpacity: 80,
         seriesToShow: 7,
         dateFormat: 'MMM dd',
         lineOptions: {
