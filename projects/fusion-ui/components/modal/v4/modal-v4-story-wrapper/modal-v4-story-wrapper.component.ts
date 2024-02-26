32a48bac75247e69b9e0f8893ae085eb74390071
AndyKIron <andy.kononenko@unity3d.com>
2024-02-26 14:33:13 +0200

    chore(isct-195): added one more story for modal

--- projects/fusion-ui/components/modal/v4/modal-v4-story-wrapper/modal-v4-story-wrapper.component.ts
+++ projects/fusion-ui/components/modal/v4/modal-v4-story-wrapper/modal-v4-story-wrapper.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ButtonComponent} from '@ironsource/fusion-ui/components/button/v4';
 import {ModalV4Component} from '../modal-v4.component';
@@ -13,6 +13,21 @@ import {TeleportingDirective} from '@ironsource/fusion-ui/directives/teleporting
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ModalV4StoryWrapperComponent implements AfterViewInit {
+    @Input() mode: string;
+
+    private defaultDialogConfig = {
+        title: 'Title',
+        description: '',
+        showHeader: true,
+        content: 'This is a dialog that demonstrates the usage of the DialogTitle and DialogActions components!',
+        primaryButtonLabel: 'Ok',
+        primaryButtonColor: 'primary',
+        secondaryButtonLabel: 'Cancel',
+        size: 'small',
+        customContent: ``
+    };
+
+    dialogConfig = {...this.defaultDialogConfig};
     modalSown = false;
     teleportTarget = '#storybook-root';
 
@@ -20,7 +35,40 @@ export class ModalV4StoryWrapperComponent implements AfterViewInit {
         this.teleportTarget = !!document.getElementById('storybook-root')?.attributes['hidden'] ? '#storybook-docs' : '#storybook-root';
     }
 
-    openModal() {
+    openModal(mode) {
+        switch (mode) {
+            case 'small':
+                this.dialogConfig = {...this.defaultDialogConfig, size: 'small'};
+                break;
+            case 'medium':
+                this.dialogConfig.size = 'large';
+                this.dialogConfig = {...this.defaultDialogConfig, size: 'medium'};
+                break;
+            case 'large':
+                this.dialogConfig.size = 'large';
+                this.dialogConfig = {...this.defaultDialogConfig, size: 'large'};
+                break;
+            case 'delete':
+                this.dialogConfig = {
+                    ...this.defaultDialogConfig,
+                    title: 'Delete this item?',
+                    primaryButtonLabel: 'Delete',
+                    primaryButtonColor: 'danger',
+                    content: '',
+                    customContent: `<div style="display: flex; flex-direction: column; gap: 8px;">Are you sure you want to delete the following item?</div><div><b>{item_name}</b></div>`
+                };
+                break;
+            case 'subTitle':
+                this.dialogConfig = {...this.defaultDialogConfig, description: 'Description'};
+                break;
+            case 'noHeader':
+                this.dialogConfig = {...this.defaultDialogConfig, showHeader: false};
+                break;
+            default:
+                this.dialogConfig = {...this.defaultDialogConfig};
+                break;
+        }
+
         this.modalSown = !this.modalSown;
     }
 
--- projects/fusion-ui/components/modal/v4/modal-v4.component.stories.ts
+++ projects/fusion-ui/components/modal/v4/modal-v4.component.stories.ts
@@ -8,7 +8,7 @@ import {ModalV4Component} from './modal-v4.component';
 import {ModalV4StoryWrapperComponent} from './modal-v4-story-wrapper/modal-v4-story-wrapper.component';
 
 export default {
-    title: 'V4/Components/Feedback/Dialog',
+    title: 'V4/Components/Feedback/Dialog/Dialog',
     component: ModalV4Component,
     decorators: [
         moduleMetadata({
@@ -21,8 +21,8 @@ export default {
         options: {
             showPanel: true,
             panelPosition: 'bottom'
-        },
-        layout: 'centered'
+        }
+        /*layout: 'centered'*/
     },
     args: {
         title: 'Title',
@@ -34,84 +34,47 @@ export default {
 
 type Story = StoryObj<ModalV4Component>;
 
-export const Basic: Story = {};
-
-export const HeaderDescription: Story = {};
-HeaderDescription.args = {
-    description: 'Description'
-};
-
-export const NoHeader: Story = {};
-NoHeader.args = {
-    showHeader: false
-};
-
-export const WithAlert: Story = {};
-WithAlert.args = {
-    alert: {
-        color: 'info',
-        description: 'Alert description',
-        action: {
-            label: 'Click me',
-            onClick: () => {
-                alert('alert action clicked');
-            }
-        }
-    }
-};
-
-export const Customized: Story = {
-    render: args => ({
-        props: {
-            showHeader: true,
-            title: 'Delete this item?',
-            content: '',
-            secondaryButtonLabel: 'Keep it',
-            primaryButtonLabel: 'Delete',
-            primaryButtonColor: 'danger'
-        },
-        template: `
-            <fusion-modal 
-                [title]="title" 
-                [description]="description" 
-                [showHeader]="showHeader"
-                [content]="content" 
-                [secondaryButtonLabel]="secondaryButtonLabel"
-                [primaryButtonLabel]="primaryButtonLabel"
-                [primaryButtonColor]="primaryButtonColor"
-              >
-                  <div>Are you sure you want to delete the following item?</div>
-                  <div style="display: flex; gap: 4px; align-items: center; margin-top: 25px;">
-                      <div class="font-v4-heading-3">Application: </div>
-                      <img style="width: 32px; height: 32px; border-radius: 6px;" alt="" loading="lazy" src="https://fusion.ironsrc.net/assets/images/v4/app_mock/Chocolate_Chip_Cookie_Game.png">
-                      <fusion-icon style="width: 16px; height: 16px;" name="v4/branded/ios"></fusion-icon> 
-                      <div class="font-v4-heading-4">Chocolate Chip Cookie Game</div>              
-                  </div>              
-            </fusion-modal>
-        `
-    })
-};
-
-export const PrimaryButtonPending: Story = {};
-PrimaryButtonPending.args = {
-    primaryButtonLoading: true
-};
-
-export const SizeMedium: Story = {};
-SizeMedium.args = {
-    size: 'medium'
-};
-
-export const SizeLarge: Story = {};
-SizeLarge.args = {
-    size: 'large'
-};
-
-export const ToggleDialog: Story = {
+export const Basic: Story = {
     render: args => ({
         props: args,
         template: `
         <fusion-modal-story-wrapper></fusion-modal-story-wrapper>  
+        `
+    })
+};
+
+export const Size: Story = {
+    render: args => ({
+        props: args,
+        template: `
+        <fusion-modal-story-wrapper mode="size"></fusion-modal-story-wrapper>  
+        `
+    })
+};
+
+export const Delete: Story = {
+    render: args => ({
+        props: args,
+        template: `
+        <fusion-modal-story-wrapper mode="delete"></fusion-modal-story-wrapper>  
+        `
+    })
+};
+
+export const SubTitle: Story = {
+    render: args => ({
+        props: args,
+        template: `
+        <fusion-modal-story-wrapper mode="subTitle"></fusion-modal-story-wrapper>  
+        `
+    })
+};
+
+export const NoHeader: Story = {
+    render: args => ({
+        props: args,
+        template: `
+        <fusion-modal-story-wrapper mode="noHeader"></fusion-modal-story-wrapper>  
         `
     })
 };
