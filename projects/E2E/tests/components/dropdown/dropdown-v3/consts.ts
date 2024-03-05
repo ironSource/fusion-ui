46f34bc5c2843a2d9e6b484828e53145f72f3ad3
Baruch Gans <baruch.gans@unity3d.com>
2024-03-05 16:45:47 +0200

    chore(isct-266): fixing dropdown tests

--- projects/E2E/tests/components/dropdown/dropdown-v3/consts.ts
+++
@@ -1,5 +0,0 @@
-import {DropdownTestIdModifiers} from '@ironsource/fusion-ui/entities';
-
-export const dropdownDefaultStoryId = 'ironsrc-components-dropdown--default';
-export const defaultTestId = 'dropdownTestId';
-export const loadedPageSelector = `[data-testid='${defaultTestId}-${DropdownTestIdModifiers.TRIGGER_BY_INDEX}']`;
