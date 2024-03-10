fd26e5f3cc42b87e51c3dbb762706451aa9fc6b0
AndyKIron <andy.kononenko@unity3d.com>
2024-03-10 09:18:42 +0200

    chore(isct-195): add "precision" to short number-scale pipe

--- projects/fusion-ui/pipes/numbers/short-number-scale-suffix/short-number-scale-suffix.pipe.spec.ts
+++ projects/fusion-ui/pipes/numbers/short-number-scale-suffix/short-number-scale-suffix.pipe.spec.ts
@@ -10,13 +10,26 @@ describe('ShortNumberScaleSuffixPipe', () => {
         const pipe = new ShortNumberScaleSuffixPipe();
         expect(pipe.transform(123000)).toBe('123 K');
     });
+    it('format number 764230.2680279986 to short string "764K"', () => {
+        const pipe = new ShortNumberScaleSuffixPipe();
+        expect(pipe.transform(764230.2680279986, {noSeparateBySpace: true, precision: 3})).toBe('764K');
+    });
+    it('format number 0.3 to short string "0.30 with precision: 3"', () => {
+        const pipe = new ShortNumberScaleSuffixPipe();
+        expect(pipe.transform(0.3, {noSeparateBySpace: true, precision: 3})).toBe('0.30');
+    });
+
     it('format number 23500 to short string "23.5 K"', () => {
         const pipe = new ShortNumberScaleSuffixPipe();
         expect(pipe.transform(23500)).toBe('23.5 K');
     });
     it('format number 3000000 to short string 3 M', () => {
         const pipe = new ShortNumberScaleSuffixPipe();
-        expect(pipe.transform(3000000)).toBe('3 M');
+        expect(pipe.transform(3000000, )).toBe('3 M');
+    });
+    it('format number 3000000 to short string 3.00 M with precision: 3', () => {
+        const pipe = new ShortNumberScaleSuffixPipe();
+        expect(pipe.transform(3000000, {precision: 3})).toBe('3.00 M');
     });
 
     it('format number 123000 to short string "123K"', () => {
