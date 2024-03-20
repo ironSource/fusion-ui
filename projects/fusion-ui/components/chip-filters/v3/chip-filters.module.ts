2656e636bed37213dfd88ab66c78c9da8a788ca1
Baruch Gans <baruch.gans@unity3d.com>
2024-03-20 18:06:59 +0200

    chore(feature/ISCT-283): revert v3 unneeded changes

--- projects/fusion-ui/components/chip-filters/v3/chip-filters.module.ts
+++ projects/fusion-ui/components/chip-filters/v3/chip-filters.module.ts
@@ -5,11 +5,10 @@ import {ChipFilterModule} from '@ironsource/fusion-ui/components/chip-filter';
 import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
 import {ReactiveFormsModule} from '@angular/forms';
 import {DropdownModule} from '@ironsource/fusion-ui/components/dropdown/v3';
-import {GenericPipe} from '@ironsource/fusion-ui';
 
 @NgModule({
     declarations: [ChipFiltersComponent],
     exports: [ChipFiltersComponent],
-    imports: [CommonModule, ChipFilterModule, IconModule, ReactiveFormsModule, DropdownModule, GenericPipe]
+    imports: [CommonModule, ChipFilterModule, IconModule, ReactiveFormsModule, DropdownModule]
 })
 export class ChipFiltersModule {}
