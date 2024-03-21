6263768e88c52dac1442a1ad5a71528aef0fe4a9
bar-unity <128602503+bar-unity@users.noreply.github.com>
2024-03-21 17:06:17 +0200

    Feature/isct 291 (#291)
    * chore(ISCT-291): create TrendIndicatorComponent tests

--- projects/fusion-ui/components/trend-indicator/v4/trend-indicator.component.spec.ts
+++ projects/fusion-ui/components/trend-indicator/v4/trend-indicator.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TrendIndicatorComponent } from './trend-indicator.component';
+import {TrendIndicatorComponent} from './trend-indicator.component';
 
 describe('TrendIndicatorComponent', () => {
-  let component: TrendIndicatorComponent;
-  let fixture: ComponentFixture<TrendIndicatorComponent>;
+    let component: TrendIndicatorComponent;
+    let fixture: ComponentFixture<TrendIndicatorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ TrendIndicatorComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TrendIndicatorComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(TrendIndicatorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TrendIndicatorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
