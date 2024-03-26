1e66328ee5f621ddf40606605ffb414272b61f1c
AndyKIron <andy.kononenko@unity3d.com>
2024-03-26 09:46:36 +0200

    chore(chart-new-colors): chart-label component changes

--- projects/fusion-ui/components/chart-labels/v4/chart-labels-v4.component.ts
+++ projects/fusion-ui/components/chart-labels/v4/chart-labels-v4.component.ts
@@ -1,9 +1,10 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChartLabel} from '@ironsource/fusion-ui/components/chart/common/base';
 import {CheckboxModule} from '@ironsource/fusion-ui/components/checkbox/v1';
 import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
 import {TooltipDirective} from '@ironsource/fusion-ui/components/tooltip/v4';
+import {ColorsService} from '@ironsource/fusion-ui/services';
 
 @Component({
     selector: 'fusion-chart-labels',
@@ -15,7 +16,25 @@ import {TooltipDirective} from '@ironsource/fusion-ui/components/tooltip/v4';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChartLabelsV4Component {
-    @Input() labels: ChartLabel[] = [];
+    @Input() set labels(value: ChartLabel[]) {
+        this._labels = value.map(label => {
+            return {
+                ...label,
+                backgroundColor: this.getLabelBGColor(label.color)
+            };
+        });
+    }
+    private _labels: ChartLabel[] = [];
+    get labels(): ChartLabel[] {
+        return this._labels;
+    }
+    @Input() bgOpacity = 10;
     @Output() labelClick = new EventEmitter<ChartLabel>();
     @Output() labelHover = new EventEmitter<ChartLabel>();
+
+    colorsService = inject(ColorsService);
+
+    getLabelBGColor(hexColor: string): string {
+        return this.colorsService.toRgba(hexColor, this.bgOpacity);
+    }
 }
