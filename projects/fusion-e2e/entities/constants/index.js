2098af1e5df431ea133921b50290c25f0919db11
Baruch Gans <baruch.gans@unity3d.com>
2024-04-04 13:37:10 +0300

    chore(build_fusion_e2e): start building

---
+++ projects/fusion-e2e/entities/constants/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GLOBAL_DEBOUNCE = exports.PAGES_PATHS = void 0;
+exports.PAGES_PATHS = {
+    OPTIMIZER_DASHBOARD: 'optimizer',
+    OPTIMIZER_DASHBOARD_CSV_MANAGER: 'optimizer-csv-manager',
+    CAMPAIGN_GENERATOR_NEW: 'generator/new',
+    MANAGEMENT: 'management'
+};
+exports.GLOBAL_DEBOUNCE = 500;
