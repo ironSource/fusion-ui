d1fd99ee71d1211c63699f212f859e0f78bd177e
Baruch Gans <baruch.gans@unity3d.com>
2024-04-18 15:50:42 +0300

    chore(fix): fix CR comment

--- projects/fusion-e2e/entities/components/inputs/base-input.ts
+++ projects/fusion-e2e/entities/components/inputs/base-input.ts
@@ -1,45 +1,39 @@
 import {InputTestIdModifiers} from '@ironsource/fusion-ui/entities/test-ids-modifiers';
-import {Locator, Page} from '@playwright/test';
-import {Editable} from '../../behavior';
-import {getTestId} from '../../global/utils';
+import {Page} from '@playwright/test';
+import {BaseElement, Editable} from '../../behavior';
+import {getTestId, getTestIdSelector} from '../../global/utils';
 
 export class BaseInputComponent extends Editable {
-    private readonly inputTestIdSelector: string;
-    private inputLocator: Locator;
+    private inputElement: BaseElement;
 
     constructor(page: Page, selector: string) {
         super(page, selector);
-        this.inputTestIdSelector = getTestId(selector, InputTestIdModifiers.FIELD);
-        this.initializeElement().then(r => r);
-    }
-
-    private async initializeElement() {
-        this.inputLocator = await this.getByTestId(this.inputTestIdSelector);
+        this.inputElement = new BaseElement(page, getTestIdSelector(getTestId(this.selector, InputTestIdModifiers.FIELD)));
     }
 
     // Get the text of the input field
     async getInputsFieldText(): Promise<string> {
-        return this.inputLocator.inputValue();
+        return this.inputElement.locator.inputValue();
     }
 
     // Get the placeholder text of the input
     async getPlaceholderText(): Promise<string> {
-        return this.inputLocator.getAttribute('placeholder');
+        return this.inputElement.locator.getAttribute('placeholder');
     }
 
     // Get the type of the input
     async getInputsType(): Promise<string> {
-        return this.inputLocator.getAttribute('type');
+        return this.inputElement.locator.getAttribute('type');
     }
 
     // Add input to the input field
     async addInput({text}): Promise<void> {
-        await this.inputLocator.type(text as string);
+        await this.inputElement.locator.type(text as string);
     }
 
     // Check if the input is disabled
     async isInputDisabled(): Promise<boolean> {
-        return this.inputLocator.isDisabled();
+        return this.inputElement.locator.isDisabled();
     }
 
     // Get the value of the input
