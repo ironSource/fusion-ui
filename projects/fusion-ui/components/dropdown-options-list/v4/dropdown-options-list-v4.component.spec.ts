4d1d1aedd6798be6bae9fcdda5ce07dc5b1dd878
AndyKIron <andy.kononenko@unity3d.com>
2024-05-23 17:36:45 +0300

    chore(isct-325): add possibility use custom component as dropdown tooltip option

--- projects/fusion-ui/components/dropdown-options-list/v4/dropdown-options-list-v4.component.spec.ts
+++ projects/fusion-ui/components/dropdown-options-list/v4/dropdown-options-list-v4.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DropdownOptionsListV4Component } from './dropdown-options-list-v4.component';
+import {TooltipComponent} from "@ironsource/fusion-ui/components/tooltip/v4";
 
 describe('DropdownOptionsListV4Component', () => {
   let component: DropdownOptionsListV4Component;
@@ -8,7 +9,7 @@ describe('DropdownOptionsListV4Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ DropdownOptionsListV4Component ]
+      imports: [ DropdownOptionsListV4Component, TooltipComponent ]
     })
     .compileComponents();
 
