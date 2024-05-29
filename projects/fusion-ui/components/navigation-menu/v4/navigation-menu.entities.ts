728029c9c7f3f169f589df7b863eb35d9ae21cc6
AndyKIron <andy.kononenko@unity3d.com>
2024-05-29 12:33:56 +0300

    chore(isct-320): add new mode hoverToShowSecondaryItems to the sidebar navigation menu
    It will show relevant secondary menu items on primary menu item hover

--- projects/fusion-ui/components/navigation-menu/v4/navigation-menu.entities.ts
+++ projects/fusion-ui/components/navigation-menu/v4/navigation-menu.entities.ts
@@ -1,7 +1,7 @@
 import {MenuItem} from '@ironsource/fusion-ui/components/menu/common/base';
 import {IconData} from '@ironsource/fusion-ui/components/icon/common/entities';
 
-export type PrimaryMenuMode = 'clickToDefaultSecondaryItem' | 'clickToOpenSecondaryMenu';
+export type PrimaryMenuMode = 'clickToDefaultSecondaryItem' | 'clickToOpenSecondaryMenu' | 'hoverToShowSecondaryItems';
 
 export enum NavigationBarItemType {
     Home = 'home',
