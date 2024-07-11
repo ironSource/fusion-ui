891a28177039af4bd48025b5af8cb658bdc15fca
AndyKIron <andy.kononenko@unity3d.com>
2024-07-11 12:28:21 +0300

    chore(components-v4): set use http client to fetch api instead xml http request

--- projects/fusion-ui/components/svg/svg.module.ts
+++ projects/fusion-ui/components/svg/svg.module.ts
@@ -3,12 +3,13 @@ import {CommonModule} from '@angular/common';
 import {SvgComponent} from './svg.component';
 import {SvgOptions} from './svg-entities';
 import {SVG_OPTIONS_DEFAULT_VALUES, SVG_OPTIONS_TOKEN} from './svg-config';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, provideHttpClient, withFetch} from '@angular/common/http';
 
 @NgModule({
     declarations: [SvgComponent],
     exports: [SvgComponent],
-    imports: [CommonModule, HttpClientModule]
+    imports: [CommonModule, HttpClientModule],
+    providers: [provideHttpClient(withFetch())]
 })
 export class SvgModule {
     static forRoot(options?: SvgOptions): ModuleWithProviders<SvgModule> {
