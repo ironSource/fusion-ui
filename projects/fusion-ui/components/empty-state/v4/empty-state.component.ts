89595036e08c53cac7eebd98a13cbb0dde67939c
AndyKIron <andy.kononenko@unity3d.com>
2024-07-28 16:25:38 +0300

    feat(isct-161): added empty-state component v4

---
+++ projects/fusion-ui/components/empty-state/v4/empty-state.component.ts
@@ -0,0 +1,18 @@
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {EmptyStateType} from '@ironsource/fusion-ui/components/empty-state/v4/empty-state.entities';
+import {SvgModule} from '@ironsource/fusion-ui/components/svg';
+import {IconModule} from '@ironsource/fusion-ui/components/icon/v1';
+
+@Component({
+    selector: 'fusion-empty-state',
+    standalone: true,
+    imports: [SvgModule, IconModule],
+    templateUrl: './empty-state.component.html',
+    styleUrl: './empty-state.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmptyStateComponent {
+    @Input() title: string;
+    @Input() description: string;
+    @Input() type: EmptyStateType = 'empty';
+}
---
+++ projects/fusion-ui/components/empty-state/v4/empty-state.entities.ts
@@ -0,0 +1,1 @@
+export type EmptyStateType = 'empty' | 'error' | 'accessDenied' | 'noResult' | 'noData' | 'chart' | 'files' | 'settings';
