bc2c54fde21408837fcae4536b13c372668f84d8
AndyKIron <andy.kononenko@unity3d.com>
2024-10-08 14:19:42 +0300

    chore(material-cdk): check virtual scroll

--- projects/fusion-ui/components/dropped-list/v4/dropped-list.component.ts
+++ projects/fusion-ui/components/dropped-list/v4/dropped-list.component.ts
@@ -2,12 +2,13 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {DroppedListOption} from './dropped-list.entities';
 import {FlagComponent} from '@ironsource/fusion-ui/components/flag/v4';
 import {TooltipDirective} from '@ironsource/fusion-ui/components/tooltip/v4';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
     selector: 'fusion-dropped-list',
     standalone: true,
     host: {class: 'fusion-v4'},
-    imports: [FlagComponent, TooltipDirective],
+    imports: [FlagComponent, TooltipDirective, ScrollingModule],
     templateUrl: './dropped-list.component.html',
     styleUrl: './dropped-list.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
