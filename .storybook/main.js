1207c3262a6bff57ac2e633dff939a62c5420434
AndyKIron <andy.kononenko@unity3d.com>
2024-11-25 11:21:01 +0200

    Revert "chore(angular-19): removed storybook"
    This reverts commit 0cde78c16c42136d957baaec2ef483e3c5259e55.

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/angular').StorybookConfig } */
+const config = {
+    stories: ['../projects/fusion-ui/components/Introduction.mdx', '../projects/fusion-ui/**/*.mdx', '../projects/fusion-ui/**/*.stories.ts'],
+    staticDirs: ['../public'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@ironsource/storybook-addon-stackblitz',
+        '@storybook/addon-mdx-gfm'
+    ],
+    framework: {
+        name: '@storybook/angular'
+    },
+    docs: {
+        /*autodocs: true // auto generate mdx dox story*/
+        autodocs: 'tag' // autogenerate only for stories with tag: 'autodoc'
+    }
+};
+
+export default config;
---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'IronSource Fusion UI',
+  brandUrl: 'https://fusion.ironsrc.net',
+  brandImage:
+    'https://fusion.ironsrc.net/assets/images/v4/unity-is-logo.svg',
+});
