85fb24155f91ad4656298809147d53777a0d1a89
AndyKIron <andy.kononenko@unity3d.com>
2024-11-26 10:01:21 +0200

    chore(sb-8): fix story for button v4

--- projects/fusion-ui/components/button/v4/button/button.component.ts
+++ projects/fusion-ui/components/button/v4/button/button.component.ts
@@ -7,6 +7,7 @@ import {GenericPipe} from '@ironsource/fusion-ui/pipes/generic';
 
 @Component({
     selector: 'fusion-button',
+    standalone: true, // todo: just for storybook to work, will be fixed in storybook 8.5.0
     imports: [CommonModule, IconModule, LoaderComponent, GenericPipe],
     templateUrl: './button.component.html',
     styleUrls: ['./button.component.scss'],
