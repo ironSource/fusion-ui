7029c0d6acc0787a0bed3e6e4ca10ba730ccf42c
AndyKIron <andy.kononenko@unity3d.com>
2024-11-26 14:50:17 +0200

    chore(sb-8): fix story for form card v4

--- projects/fusion-ui/components/form-card/v4/form-card.component.ts
+++ projects/fusion-ui/components/form-card/v4/form-card.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
     selector: 'fusion-form-card',
+    standalone: true, // todo: just for storybook to work, will be fixed in storybook 8.5.0
     host: {class: 'fusion-v4'},
     imports: [],
     templateUrl: './form-card.component.html',
