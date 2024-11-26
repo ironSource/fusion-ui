b4f40735dd9dd666a64b2dc1c57a6aff253beee3
AndyKIron <andy.kononenko@unity3d.com>
2024-11-26 14:49:24 +0200

    chore(sb-8): fix story for flag v4

--- projects/fusion-ui/components/flag/v4/flag.component.ts
+++ projects/fusion-ui/components/flag/v4/flag.component.ts
@@ -4,6 +4,7 @@ import {FlagBaseComponent} from './flag-base.component';
 
 @Component({
     selector: 'fusion-flag',
+    standalone: true, // todo: just for storybook to work, will be fixed in storybook 8.5.0
     imports: [CommonModule],
     template: `
         <svg class="fu-flag" focusable="false" aria-hidden="true" viewBox="0 0 24 24" [style.border-radius]="borderRadius">
