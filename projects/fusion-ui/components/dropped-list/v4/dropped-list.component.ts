115f631cb2492a68ae01eed29da5bfbaacfe1e37
AndyKIron <andy.kononenko@unity3d.com>
2024-11-26 14:47:10 +0200

    chore(sb-8): fix story for dropped list v4

--- projects/fusion-ui/components/dropped-list/v4/dropped-list.component.ts
+++ projects/fusion-ui/components/dropped-list/v4/dropped-list.component.ts
@@ -5,6 +5,7 @@ import {TooltipDirective} from '@ironsource/fusion-ui/components/tooltip/v4';
 
 @Component({
     selector: 'fusion-dropped-list',
+    standalone: true, // todo: just for storybook to work, will be fixed in storybook 8.5.0
     host: {class: 'fusion-v4'},
     imports: [FlagComponent, TooltipDirective],
     templateUrl: './dropped-list.component.html',
