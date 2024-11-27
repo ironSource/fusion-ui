4cfee44815d44bd3258364699447faec4c3922e3
AndyKIron <andy.kononenko@unity3d.com>
2024-11-27 16:39:20 +0200

    chore(sb-8): fix story for tag v3

--- projects/fusion-ui/components/tag/v3/tag.component.ts
+++ projects/fusion-ui/components/tag/v3/tag.component.ts
@@ -7,6 +7,7 @@ import {TooltipModule} from '@ironsource/fusion-ui/components/tooltip/v3';
 
 @Component({
     selector: 'fusion-tag',
+    standalone: true, // todo: just for storybook to work, will be fixed in storybook 8.5.0
     templateUrl: './tag.component.html',
     styleUrls: ['./tag.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
